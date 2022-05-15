f4ac1e1c9fd41f29660ee2e7833fe14be7a2701c
Martin Miglio <marmig0404@gmail.com>
2022-05-14 23:30:34 -0400

    moved card stuff to card added methods for finding multiple references moved compare_pixels to pixel_is_equal

--- pyclashbot/image_rec.py
+++ pyclashbot/image_rec.py
@@ -1,14 +1,68 @@
 import cv2
 import numpy as np
+from typing import Union
+from PIL import Image
+from os.path import join
 
-def compare_images(image, template, threshold=0.8):
+
+def find_references(screenshot: Union[np.ndarray, Image.Image], folder: str, names: list[str], tolerance=0.97):
+    """find reference images in a screenshot
+
+    Args:
+        screenshot (Union[np.ndarray, Image.Image]): find references in screenshot
+        folder (str): folder to find references (from within reference_images)
+        names (list[str]): names of references
+        tolerance (float, optional): tolerance. Defaults to 0.97.
+
+    Returns:
+        list[Union[tuple[int,int],None]: coordinate locations
+    """
+    return [find_reference(screenshot, folder, name, tolerance) for name in names]
+
+
+def find_reference(screenshot: Union[np.ndarray, Image.Image], folder: str, name: str, tolerance=0.97):
+    """find a reference image in a screenshot
+
+    Args:
+        screenshot (Union[np.ndarray, Image.Image]): find reference in screenshot
+        folder (str): folder to find reference (from within reference_images)
+        name (str): name of reference
+        tolerance (float, optional): tolerance. Defaults to 0.97.
+
+    Returns:
+        Union[tuple[int,int], None]: coordinate location
+    """
+    return compare_images(screenshot, Image.open(join("pyclashbot", "reference_images", folder, name)), tolerance)
+
+
+def pixel_is_equal(pix1, pix2, tol):
+    """check pixel equality
+
+    Args:
+        pix1 (list[int]): [R,G,B] pixel
+        pix2 (list[int]): [R,G,B] pixel
+        tol (float): tolerance
+
+    Returns:
+        bool: are pixels equal
+    """
+    diff_r = abs(pix1[0] - pix2[0])
+    diff_g = abs(pix1[1] - pix2[1])
+    diff_b = abs(pix1[2] - pix2[2])
+    if (diff_r < tol) and (diff_g < tol) and (diff_b < tol):
+        return True
+    else:
+        return False
+
+
+def compare_images(image: Union[np.ndarray, Image.Image], template: Union[np.ndarray, Image.Image], threshold=0.8):
     """detects pixel location of a template in an image
     Args:
-        image (Image): image to find template within
-        template (Image): template image to match to
+        image (Union[np.ndarray, Image.Image]): image to find template within
+        template (Union[np.ndarray, Image.Image]): template image to match to
         threshold (float, optional): matching threshold. defaults to 0.8
     Returns:
-        tuple[(int,int)] or None: a tuple of pixel location (x,y)
+        Union[tuple[int,int], None]: a tuple of pixel location (x,y)
     """
 
     # show template
@@ -31,4 +85,4 @@ def compare_images(image, template, threshold=0.8):
     if len(loc[0]) != 1:
         return None
 
-    return (loc[0][0], loc[1][0])
+    return (int(loc[0][0]), int(loc[1][0]))
