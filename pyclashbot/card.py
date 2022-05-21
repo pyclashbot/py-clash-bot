6a1db2b2e17686f01e1b94ac235a6ba9b4da1dda
Martin Miglio <marmig0404@gmail.com>
2022-05-21 14:53:34 -0400

    removed check_key_presses import from __main__

--- pyclashbot/card.py
+++ pyclashbot/card.py
@@ -6,7 +6,6 @@ from inspect import getmembers, isfunction
 import numpy as np
 import pyautogui
 
-from pyclashbot.__main__ import check_quit_key_press
 from pyclashbot.image_rec import find_references
 
 
@@ -124,7 +123,6 @@ def check_if_hero_ability_is_available():
 
 
 def fight_with_deck_list(enemy_troop_position):
-    check_quit_key_press()
 #check for hero abilities
     ability_coords = check_if_hero_ability_is_available()
     if ability_coords:
