8410573a9bbdd64e1ceeb4c6690ef716ad0e9df1
Matthew Miglio <matmigg0804@gmail.com>
2022-05-22 20:55:57 -0400

    dunno

--- pyclashbot/chest.py
+++ pyclashbot/chest.py
@@ -32,6 +32,92 @@ def check_if_unlock_chest_button_exists():
     return False
 
 
+def check_if_has_chest_unlocking():
+    current_image = screenshot()
+    reference_folder = "unlocking_chest_images"
+    references = [
+        "1.png",
+        "2.png",
+        "3.png",
+        "4.png",
+        "5.png",
+        "6.png",
+        "7.png",
+        "8.png",
+        "9.png",
+        "10.png",
+        "11.png",
+        "12.png",
+        "13.png",
+        "14.png",
+        "15.png",
+        "16.png",
+        "17.png",
+        "18.png",
+        "19.png",
+        "20.png",
+        "21.png",
+        "22.png",
+        "23.png",
+        "24.png",
+        "25.png",
+        "26.png",
+        "27.png",
+        "28.png",
+        "29.png",
+        "30.png",
+        "31.png",
+        "32.png",
+        "33.png",
+        "34.png",
+        "35.png",
+        "36.png",
+        "37.png",
+        "38.png",
+        "39.png",
+        "40.png",
+        "41.png",
+        "42.png",
+        "43.png",
+        "44.png",
+        "45.png",
+        "46.png",
+        "47.png",
+        "48.png",
+        "49.png",
+        "50.png",
+        "51.png",
+        "52.png",
+        "53.png",
+        "54.png",
+        "55.png",
+        "56.png",
+        "57.png",
+        "58.png",
+        "59.png",
+        "60.png",
+        "61.png",
+        "62.png",
+        "63.png",
+        "64.png",
+        "65.png",
+        "66.png",
+    ]
+
+    locations = find_references(
+        screenshot=current_image,
+        folder=reference_folder,
+        names=references,
+        tolerance=0.97
+    )
+
+    for location in locations:
+        if location is not None:
+            # found a location
+            return True
+    return False
+
+
 def open_chests(logger):
     logger.log("clicking chest1")
     click(78, 554)
