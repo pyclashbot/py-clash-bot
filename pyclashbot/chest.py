8dcab098780024700f49d015666d3eaf1087ce88
Martin Miglio <marmig0404@gmail.com>
2022-05-23 13:27:41 -0400

    updated chest unlocking detection

--- pyclashbot/chest.py
+++ pyclashbot/chest.py
@@ -33,74 +33,14 @@ def check_if_unlock_chest_button_exists():
 
 
 def look_for_clock():
-    current_image = screenshot(region=(35,490,360,40))
+    current_image = screenshot(region=(35, 490, 360, 40))
     reference_folder = "unlocking_chest_images"
     references = [
-        "clock1.png",
-        "clock2.png",
-        "clock3.png",
-        "clock4.png",
-        "clock5.png",
-        "clock6.png",
-        "clock7.png",
-        "clock8.png",
-        "clock9.png",
-        "clock10.png",
-        "clock11.png",
-        "clock12.png",
-        "clock13.png",
-        "clock14.png",
-        "clock15.png",
-        "clock16.png",
-        "clock17.png",
-        "clock18.png",
-        "clock19.png",
-        "clock20.png",
-        "clock21.png",
-        "clock22.png",
-        "clock23.png",
-        "clock24.png",
-        "clock25.png",
-        "clock26.png",
-        "clock27.png",
-        "clock28.png",
-        "clock29.png",
-        "clock30.png",
-        "clock31.png",
-        "clock32.png",
-        "clock33.png",
-        "clock34.png",
-        "clock35.png",
-        "clock36.png",
-        "clock37.png",
-        "clock38.png",
-        "clock39.png",
-        "clock40.png",
-        "clock41.png",
-        "clock42.png",
-        "clock43.png",
-        "clock44.png",
-        "clock45.png",
-        "clock46.png",
-        "clock47.png",
-        "clock48.png",
-        "clock49.png",
-        "clock50.png",
-        "clock51.png",
-        "clock52.png",
-        "clock53.png",
-        "clock54.png",
-        "clock55.png",
-        "clock56.png",
-        "clock57.png",
-        "clock58.png",
-        "clock59.png",
-        "clock60.png",
-        "clock61.png",
-        "clock62.png",
-        "clock63.png",
-        "clock64.png",
-        "clock65.png",
+        "chest_unlocking_1.png",
+        "chest_unlocking_2.png",
+        "chest_unlocking_3.png",
+        "chest_unlocking_4.png",
+        "chest_unlocking_5.png",
     ]
 
     locations = find_references(
@@ -118,14 +58,14 @@ def look_for_clock():
 
 
 def check_if_has_chest_unlocking():
-    n=5
+    n = 5
     while n != 0:
         if look_for_clock():
             return True
         else:
-            n=n-1
+            n = n-1
             time.sleep(0.2)
-        
+
 
 def open_chests(logger):
     logger.log("clicking chest1")
