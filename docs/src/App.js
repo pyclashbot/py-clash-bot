01b2922f39ecbe62699b41726c26d61b220003b7
Martin Miglio <marmig0404@gmail.com>
2022-05-25 02:30:11 -0400

    added eslint google formatting

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import memu_display_settings from './assets/memu_display_settings.png';
-import memu_instace_settings from './assets/memu_instance_settings.png';
-import memu_appearance_settings from './assets/memu_appearance_settings.png';
+import React from 'react';
+import memuDisplaySettings from './assets/memu_display_settings.png';
+import memuInstaceSettings from './assets/memu_instance_settings.png';
+import memuAppearanceSettings from './assets/memu_appearance_settings.png';
 import SourceBadge from './components/badge/SourceBadge';
 import TestBadge from './components/badge/TestBadge';
 import ReleaseLink from './components/release/ReleaseLink';
 import ReleaseUpdate from './components/release/ReleaseUpdate';
 import ReleaseDownloadCount from './components/release/ReleaseDownloadCount';
-import { initializeGA } from './GoogleAnalytics';
+import {initializeGA} from './GoogleAnalytics';
 
-export default function App()
-{
+/**
+ * main app
+ * @return {Component} App
+ */
+export default function App() {
   initializeGA();
   return (
     <div className="mume markdown-preview  ">
@@ -25,23 +29,35 @@ export default function App()
       <p>Choose one of the install methods below.</p>
       <h4 className="mume-header" id="a-windows-install">a. Windows Install</h4>
       <ReleaseLink />
-      <p>Release last updated <ReleaseUpdate /> with <ReleaseDownloadCount /> downloads.</p>
-      <h4 className="mume-header" id="b-install-from-source">b. Install with pip</h4>
+      <p><ReleaseUpdate />
+        with <ReleaseDownloadCount /> downloads.</p>
+      <h4 className="mume-header" id="b-install-from-source">
+        b. Install with pip
+      </h4>
 
       <p>Enter the following into the command line:</p>
       <code>pip install py-clash-bot</code>
 
       <h2>MEmu Dependency</h2>
-      <p>To emulate the game, <a href="https://www.memuplay.com/" rel="nofollow">Download and install MEmu</a>.</p>
+      <p>To emulate the game,
+        <a href="https://www.memuplay.com/" rel="nofollow">
+          Download and install MEmu
+        </a>
+        .
+      </p>
       <h3>Configure MEmu</h3>
-      <p>Using the Multiple Instance Manager, set the instance, display, and appearance settings of your instance to match the following</p>
-      <p><img src={memu_instace_settings}
+      <p>Using the Multiple Instance Manager, set the instance, display,
+        and appearance settings of your instance to match the following</p>
+      <p><img src={memuInstaceSettings}
         alt="MEmu instace options" /></p>
-      <p><img src={memu_display_settings}
+      <p><img src={memuDisplaySettings}
         alt="MEmu display options" /></p>
-      <p><img src={memu_appearance_settings}
+      <p><img src={memuAppearanceSettings}
         alt="MEmu appearance options" /></p>
-      <p>Then start the emulator and install Clash Royale with the Google Play Store.</p>
+      <p>
+        Then start the emulator and install
+        Clash Royale with the Google Play Store.
+      </p>
       <h2>Run py-clash-bot</h2>
       <h4>a. Installed on Windows</h4>
       <p>Run the desktop shortcut the installer created.</p>
--- docs/src/components/badge/TestBadge.js
+++ docs/src/components/badge/TestBadge.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import { testBadgeEvent } from '../../GoogleAnalytics';
+import React, {Component} from 'react';
+import {testBadgeEvent} from '../../GoogleAnalytics';
 
 /**
 * Component for latest release download link
 */
-export default class TestBadge extends Component
-{
-
-    render()
-    {
-        return (
-            <a href="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml" rel="nofollow" onClick={() => testBadgeEvent()}>
-                <img
-                    src="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml/badge.svg"
-                    alt="GitHub Python tests badge" />
-            </a>
-        );
-    }
+export default class TestBadge extends Component {
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <a href="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml" rel="nofollow" onClick={() => testBadgeEvent()}>
+        <img
+          src="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml/badge.svg"
+          alt="GitHub Python tests badge" />
+      </a>
+    );
+  }
 }
