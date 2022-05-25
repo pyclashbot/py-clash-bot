ce790604e0652ba8999372972facbd00e6b579ce
Martin Miglio <marmig0404@gmail.com>
2022-05-25 02:30:11 -0400

    added eslint google formatting

--- docs/src/components/badge/SourceBadge.js
+++ docs/src/components/badge/SourceBadge.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SourceBadgeSVG from '../../assets/source_badge.svg';
-import { sourceBadgeEvent } from '../../GoogleAnalytics';
+import {sourceBadgeEvent} from '../../GoogleAnalytics';
 
 /**
 * Component for latest release download link
 */
-export default class SourceBadge extends Component
-{
-
-    render()
-    {
-        return (
-            <a
-                href="https://github.com/matthewmiglio/py-clash-bot/"
-                rel="nofollow"
-                onClick={() => sourceBadgeEvent()}>
-                <img
-                    src={SourceBadgeSVG}
-                    alt="Link to Source on GitHub" />
-            </a>
-        );
-    }
+export default class SourceBadge extends Component {
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <a
+        href="https://github.com/matthewmiglio/py-clash-bot/"
+        rel="nofollow"
+        onClick={() => sourceBadgeEvent()}>
+        <img
+          src={SourceBadgeSVG}
+          alt="Link to Source on GitHub" />
+      </a>
+    );
+  }
 }
