b8b2b33a53734004ac34b7bc706c69883eeaa01e
Martin Miglio <marmig0404@gmail.com>
2022-05-25 02:30:11 -0400

    added eslint google formatting

--- docs/src/components/release/ReleaseLink.js
+++ docs/src/components/release/ReleaseLink.js
@@ -1,50 +1,61 @@
-import React, { Component } from 'react';
-import { releaseLinkEvent } from '../../GoogleAnalytics';
+import React, {Component} from 'react';
+import {releaseLinkEvent} from '../../GoogleAnalytics';
 
 
-
-const api_url = 'https://api.github.com/repos/matthewmiglio/py-clash-bot/releases/latest';
+const apiUrl = 'https://api.github.com/repos/matthewmiglio/py-clash-bot/releases/latest';
 
 
 /**
 * Component for latest release download link
 */
-export default class ReleaseLink extends Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            release_url: []
-        };
-    }
-
-    GetLatestReleaseInfo()
-    {
-        fetch(api_url)
-            .then(response => response.json())
-            .then(data =>
-            {
-                this.setState({ release_url: data.assets[0].browser_download_url });
-                var downloadCount = 0;
-                for (let asset in data.assets)
-                {
-                    downloadCount = downloadCount + asset.download_count;
-                }
-                this.setState({ download_count: downloadCount });
-                this.setState({ last_updated: data.assets[0].updated_at });
-            });
-    }
-    componentDidMount()
-    {
-        this.GetLatestReleaseInfo();
-    }
-    render()
-    {
-        return (
-            <p><a className='download' href={this.state.release_url} onClick={() => releaseLinkEvent()}>
+export default class ReleaseLink extends Component {
+  /**
+   * constructor to set states
+   * @param {*} props component props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      release_url: [],
+    };
+  }
+  /**
+   * make api call to get download count on release
+   */
+  getLatestReleaseInfo() {
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({release_url: data.assets[0].browser_download_url});
+          let downloadCount = 0;
+          for (const asset in data.assets) {
+            if (Object.prototype.hasOwnProperty.call(data, assets)) {
+              downloadCount = downloadCount + asset.download_count;
+            }
+          }
+          this.setState({download_count: downloadCount});
+          this.setState({last_updated: data.assets[0].updated_at});
+        });
+  }
+  /**
+   * on mount get release info
+   */
+  componentDidMount() {
+    this.getLatestReleaseInfo();
+  }
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <p><a
+        className='download'
+        href={this.state.release_url}
+        onClick={() => releaseLinkEvent()}
+      >
                 Download the latest release
-            </a></p>
-        );
-    }
+      </a></p>
+    );
+  }
 }
