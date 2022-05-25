0e09bbe5019511c2f06f565bda094b4d690ff986
Martin Miglio <marmig0404@gmail.com>
2022-05-24 22:23:35 -0400

    updated page

--- docs/src/App.js
+++ docs/src/App.js
@@ -23,11 +23,9 @@ export default function App()
       <h2 className="mume-header" id="install">Install</h2>
 
       <p>Choose one of the install methods below.</p>
-      <p>Release last updated <ReleaseUpdate /> with <ReleaseDownloadCount /> downloads.</p>
       <h4 className="mume-header" id="a-windows-install">a. Windows Install</h4>
-
       <ReleaseLink />
-
+      <p>Release last updated <ReleaseUpdate /> with <ReleaseDownloadCount /> downloads.</p>
       <h4 className="mume-header" id="b-install-from-source">b. Install with pip</h4>
 
       <p>Enter the following into the command line:</p>
