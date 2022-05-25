1d49e4a254c7899bad83870ea2e66691abdd3935
Martin Miglio <marmig0404@gmail.com>
2022-05-25 02:30:11 -0400

    added eslint google formatting

--- docs/src/components/release/ReleaseUpdate.js
+++ docs/src/components/release/ReleaseUpdate.js
@@ -1,44 +1,56 @@
-import { Component } from 'react';
+import {Component} from 'react';
 
 
-const api_url = 'https://api.github.com/repos/matthewmiglio/py-clash-bot/releases/latest';
+const apiUrl = 'https://api.github.com/repos/matthewmiglio/py-clash-bot/releases/latest';
 
 
 /**
 * Component for time since last release
 */
-export default class ReleaseUpdate extends Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            last_updated: []
-        };
-    }
+export default class ReleaseUpdate extends Component {
+  /**
+   * constructor to set states
+   * @param {*} props component props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      last_updated: [],
+    };
+  }
 
-    GetLatestReleaseInfo()
-    {
-        fetch(api_url)
-            .then(response => response.json())
-            .then(data =>
-            {
-                const oneHour = 60 * 60 * 1000;
-                const oneDay = 24 * oneHour;
-                var dateDiff = new Date() - new Date(data.assets[0].updated_at);
-                // eslint-disable-next-line eqeqeq
-                var timeAgo = dateDiff < oneDay ? `${(dateDiff / oneHour).toFixed()} ${(dateDiff / oneHour).toFixed() == 1 ? 'hour' : 'hours'} ago` : `${(dateDiff / oneDay).toFixed()} days ago`;
-                this.setState({ last_updated: timeAgo });
-            });
-    }
-    componentDidMount()
-    {
-        this.GetLatestReleaseInfo();
-    }
-    render()
-    {
-        return (
-            this.state.last_updated
-        );
-    }
+  /**
+   * make api call to get time since release
+   */
+  getLatestReleaseInfo() {
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const oneHour = 60 * 60 * 1000;
+          const oneDay = 24 * oneHour;
+          const dateDiff = new Date() - new Date(data.assets[0].updated_at);
+          // eslint-disable-next-line eqeqeq
+          let daysAgo = (dateDiff / oneDay).toFixed();
+          daysAgo = `${daysAgo} ${daysAgo == 1 ? 'day' : 'days'} ago`;
+          let hoursAgo = (dateDiff / oneHour).toFixed();
+          hoursAgo = `${hoursAgo} ${hoursAgo == 1 ? 'hour' : 'hours'} ago`;
+          const timeAgo = dateDiff < oneDay ? hoursAgo : daysAgo;
+          this.setState({last_updated: timeAgo});
+        });
+  }
+  /**
+   * on mount get release info
+   */
+  componentDidMount() {
+    this.getLatestReleaseInfo();
+  }
+  /**
+   * react render override
+   * @return {string}
+   */
+  render() {
+    return (
+      `Release last updated ${this.state.last_updated} `
+    );
+  }
 }
