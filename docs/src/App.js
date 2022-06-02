c9bb1b1c7d1081814365c2e146331f25effd13b3
Martin Miglio <marmig0404@gmail.com>
2022-06-02 10:56:46 -0400

    extracted memu dependency component

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import memuAppearanceSettings from './assets/memu_appearance_settings.png';
-import memuDisplaySettings from './assets/memu_display_settings.png';
-import memuInstaceSettings from './assets/memu_instance_settings.png';
 import CodeFactorBadge from './components/badge/CodeFactorBadge';
 import PyPiBadge from './components/badge/PyPiBadge';
 import SourceBadge from './components/badge/SourceBadge';
 import TestBadge from './components/badge/TestBadge';
+import MEmuDependency from './components/instruction/MEmuDependency';
 import ReleaseDownloadCount from './components/release/ReleaseDownloadCount';
 import ReleaseLink from './components/release/ReleaseLink';
 import ReleaseUpdate from './components/release/ReleaseUpdate';
@@ -41,27 +39,7 @@ export default function App() {
 
       <p>Enter the following into the command line:</p>
       <code>pip install py-clash-bot</code>
-
-      <h2>MEmu Dependency</h2>
-      <p>To emulate the game,
-        <a href="https://www.memuplay.com/" rel="nofollow">
-          Download and install MEmu
-        </a>
-        .
-      </p>
-      <h3>Configure MEmu</h3>
-      <p>Using the Multiple Instance Manager, set the instance, display,
-        and appearance settings of your instance to match the following</p>
-      <p><img src={memuInstaceSettings}
-        alt="MEmu instace options" /></p>
-      <p><img src={memuDisplaySettings}
-        alt="MEmu display options" /></p>
-      <p><img src={memuAppearanceSettings}
-        alt="MEmu appearance options" /></p>
-      <p>
-        Then start the emulator and install
-        Clash Royale with the Google Play Store.
-      </p>
+      <MEmuDependency/>
       <h2>Run py-clash-bot</h2>
       <h4>a. Installed on Windows</h4>
       <p>Run the desktop shortcut the installer created.</p>
