c5deaa52a63b67f7db0bda31d61daa4ca8653aef
Martin Miglio <marmig0404@gmail.com>
2022-06-02 12:18:55 -0400

    extracted components from App.js

--- docs/src/components/readme/Dependencies.js
+++ docs/src/components/readme/Dependencies.js
@@ -5,7 +5,7 @@ import memuDisplaySettings from '../../assets/memu_display_settings.png';
 import memuInstaceSettings from '../../assets/memu_instance_settings.png';
 
 /**
-* Component for latest release download link
+* Component for memu dependency instructions
 */
 export default class MEmuDependency extends Component {
   /**
@@ -14,7 +14,7 @@ export default class MEmuDependency extends Component {
    */
   render() {
     return (
-      <div className='memu-dependency'>
+      <div className='dependencies'>
         <h2>MEmu Dependency</h2>
         <p>To emulate the game, <a href="https://www.memuplay.com/" rel="nofollow" onClick={()=>memuDownloadEvent()}>
             Download and install MEmu</a>.
@@ -22,12 +22,18 @@ export default class MEmuDependency extends Component {
         <h3>Configure MEmu</h3>
         <p>Using the Multiple Instance Manager, set the instance, display,
           and appearance settings of your instance to match the following</p>
-        <p><img src={memuInstaceSettings}
-          alt="MEmu instace options" /></p>
-        <p><img src={memuDisplaySettings}
-          alt="MEmu display options" /></p>
-        <p><img src={memuAppearanceSettings}
-          alt="MEmu appearance options" /></p>
+        <p><img
+          src={memuInstaceSettings}
+          alt="MEmu instace options"
+          style={{mixBlendMode: 'lighten'}}/></p>
+        <p><
+          img src={memuDisplaySettings}
+          alt="MEmu display options"
+          style={{mixBlendMode: 'lighten'}}/></p>
+        <p><
+          img src={memuAppearanceSettings}
+          alt="MEmu appearance options"
+          style={{mixBlendMode: 'lighten'}} /></p>
         <p>
           Then start the emulator and install
           Clash Royale with the Google Play Store.
---
+++ docs/src/components/readme/Description.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import CodeFactorBadge from '../badge/CodeFactorBadge';
+import PyPiBadge from '../badge/PyPiBadge';
+import SourceBadge from '../badge/SourceBadge';
+import TestBadge from '../badge/TestBadge';
+
+/**
+* Component for project description
+*/
+export default class Description extends Component {
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <div className='description'>
+        <h1 className="mume-header" id="py-clash-bot">py-clash-bot</h1>
+        <h2>
+          <SourceBadge />
+          <PyPiBadge />
+          <TestBadge />
+          <CodeFactorBadge />
+        </h2>
+        <p>A Clash Royale automation bot written in Python.</p>
+      </div>
+    );
+  }
+}
