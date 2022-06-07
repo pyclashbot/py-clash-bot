630220830a3c5b337c0d4ca429bad2b13cb6bdd1
Martin Miglio <marmig0404@gmail.com>
2022-06-07 17:29:26 -0400

    only test msi compile on windows

--- tests/compile_test.py
+++ tests/compile_test.py
@@ -1,9 +1,12 @@
+from platform import system
 import unittest
-from setuptools import sandbox 
+from setuptools import sandbox
+
 
 class CompileTest(unittest.TestCase):
     def test_windows_compile(self):
-        sandbox.run_setup('setup_msi.py', ['bdist_msi'])
+        if system() == 'Windows':
+            sandbox.run_setup('setup_msi.py', ['bdist_msi'])
 
     def test_wheel_compile(self):
         sandbox.run_setup('setup.py', ['sdist'])
