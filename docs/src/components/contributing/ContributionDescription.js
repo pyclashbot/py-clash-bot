c93ac562e0098205f911a11aedd9cffe38d023a7
Martin Miglio <marmig0404@gmail.com>
2022-06-10 18:19:18 -0400

    Added page for info on contributing

---
+++ docs/src/components/contributing/ContributionDescription.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import CodeFactorBadge from '../badge/CodeFactorBadge';
+import PyPiBadge from '../badge/PyPiBadge';
+import SourceBadge from '../badge/SourceBadge';
+import TestBadge from '../badge/TestBadge';
+
+/**
+* Component for Contribution description
+*/
+export default class ContributionDescription extends Component {
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <div className='description'>
+        <h1 className="mume-header" id="py-clash-bot">Contributing</h1>
+        <h2>
+          <SourceBadge />
+          <PyPiBadge />
+          <TestBadge />
+          <CodeFactorBadge />
+        </h2>
+        <p>
+            To contribute to this project,
+            either open a bug report, a feature request or a pull request
+        </p>
+      </div>
+    );
+  }
+}
