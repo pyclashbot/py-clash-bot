eb517228ff0f85465aea1fd3079cea32062b36a0
Matthew Miglio <matmigg0804@gmail.com>
2022-06-10 19:41:07 -0400

    dunno

--- ml/manual_classify.py
+++ ml/manual_classify.py
@@ -10,6 +10,7 @@ from PIL import Image
 
 
 def prepare_files(classes):
+    print("prepping files")
     """make the necessary directories
 
     Args:
@@ -42,6 +43,7 @@ def prepare_files(classes):
 
 
 def crop_image(file_name: str) -> list[str]:
+    print(f"Cropping {file_name[70:]}")
     """make cropped images
 
     Args:
@@ -71,6 +73,7 @@ def crop_image(file_name: str) -> list[str]:
 
 
 def crop_images(file_names: list[str]) -> dict[str, list[str]]:
+    print(f"Cropping images")
     """crop a list of images
 
     Args:
