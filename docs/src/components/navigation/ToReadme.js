7159c1138ec1349df6ca77b183f6a0e1c9005755
Martin Miglio <marmig0404@gmail.com>
2022-06-10 18:19:18 -0400

    Added page for info on contributing

---
+++ docs/src/components/navigation/ToReadme.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+/**
+* Component for NavToReadme
+*/
+export default class NavToReadme extends Component {
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <div className='nav'>
+        <Link to='/' >Back to home</Link>
+      </div>
+    );
+  }
+}
