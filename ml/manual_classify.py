213ea36d439b19b1ce448295af300daab7fecc96
Martin Miglio <marmig0404@gmail.com>
2022-06-10 15:51:47 -0400

    multiprocessing for image cropping should help with reducing processing time with large workloads

--- ml/manual_classify.py
+++ ml/manual_classify.py
@@ -3,20 +3,10 @@ import os
 import sys
 import time
 from io import BytesIO
+import multiprocessing
 
 import PySimpleGUI as sg
 from PIL import Image
-
-# classes for which to make folders
-classes = ["A1", "A3", "B1", "B3", "C1", "C2", "C3", "None"]
-
-# locations of corners on grid
-corners = {
-    #name:(left, top, right, bottom)
-    "A1": (65, 190, 175, 255),    "A2": (175, 190, 245, 255),    "A3": (245, 190, 355, 255),
-    "B1": (65, 275, 175, 400),    "B2": (175, 275, 245, 400),    "B3": (245, 275, 355, 400),
-    "C1": (65, 400, 175, 515),    "C2": (175, 400, 245, 515),    "C3": (245, 400, 355, 515),
-}
 
 
 def prepare_files(classes):
@@ -51,30 +41,60 @@ def prepare_files(classes):
     return train_data_dir, file_names
 
 
-def make_corners(file_name, corners) -> list[str]:
+def crop_image(file_name: str) -> list[str]:
     """make cropped images
 
     Args:
         file_name (str): location of folder
-        corners (tuple[int]): the left, top, right, and bottom coordinates
 
     Returns:
         list[str]: list of cropped images
     """
+    # locations of corners on grid
+    corners = {
+        #name:(left, top, right, bottom)
+        "A1": (65, 190, 175, 255),    "A2": (175, 190, 245, 255),    "A3": (245, 190, 355, 255),
+        "B1": (65, 275, 175, 400),    "B2": (175, 275, 245, 400),    "B3": (245, 275, 355, 400),
+        "C1": (65, 400, 175, 515),    "C2": (175, 400, 245, 515),    "C3": (245, 400, 355, 515),
+    }
     crops = []
     for key in corners:
-        im_file = BytesIO()
+        # crop image
         crop = Image.open(file_name).crop(corners[key])
+        # convert image to b64 for use in button image
+        im_file = BytesIO()
         crop.save(im_file, format="PNG")
         crop = base64.b64encode(im_file.getvalue())
         crops.append(crop)
     return crops
 
 
-def prompt_for_class(file_name: str, corner_images: list[str]):
-    """prompt user for classes
+def crop_images(file_names: list[str]) -> dict[str, list[str]]:
+    """crop a list of images
 
     Args:
+        file_names (list[str]): list of image paths
+
+    Returns:
+        dict[str,list[str]]: dictionary of image paths and their crops in b64
+    """
+    try:
+        cpus = multiprocessing.cpu_count()
+    except NotImplementedError:
+        cpus = 2   # arbitrary default
+
+    pool = multiprocessing.Pool(processes=cpus)
+    image_crops = pool.map(crop_image, file_names)
+
+    image_crop_dict = dict(zip(file_names, image_crops))
+    return image_crop_dict
+
+
+def prompt_for_class(file_name: str, image_crops: list[str]) -> str:
+    """prompt user to classify image
+
+    Args:
+        file_name (str): name of file to be classified
         corner_images (list[str]): list of crops of image b64 encoded
 
     Returns:
@@ -83,21 +103,22 @@ def prompt_for_class(file_name: str, corner_images: list[str]):
     # define GUI layout
     layout = [
         [
-            sg.Button("A1", image_data=corner_images[0]),
-            sg.Button("A2", image_data=corner_images[1]),
-            sg.Button("A3", image_data=corner_images[2])
+            sg.Button("A1", image_data=image_crops[0]),
+            sg.Button("A2", image_data=image_crops[1]),
+            sg.Button("A3", image_data=image_crops[2])
         ],
         [
-            sg.Button("B1", image_data=corner_images[3]),
-            sg.Button("B2", image_data=corner_images[4]),
-            sg.Button("B3", image_data=corner_images[5])
+            sg.Button("B1", image_data=image_crops[3]),
+            sg.Button("B2", image_data=image_crops[4]),
+            sg.Button("B3", image_data=image_crops[5])
         ],
         [
-            sg.Button("C1", image_data=corner_images[6]),
-            sg.Button("C2", image_data=corner_images[7]),
-            sg.Button("C3", image_data=corner_images[8])
+            sg.Button("C1", image_data=image_crops[6]),
+            sg.Button("C2", image_data=image_crops[7]),
+            sg.Button("C3", image_data=image_crops[8])
         ],
-        [sg.Button("Exit"), sg.Text(file_name, size=(27,None)), sg.Button("None")]
+        [sg.Button("Exit"), sg.Text(
+            file_name, size=(27, None)), sg.Button("None")]
     ]
     # define GUI window
     window = sg.Window(
@@ -116,7 +137,7 @@ def prompt_for_class(file_name: str, corner_images: list[str]):
         return event
 
 
-def save_classified_image(classes, train_data_dir, file_name, classification):
+def save_classified_image(classes: list[str], train_data_dir: str, file_name: str, classification: str) -> None:
     """save image to proper class folder
 
     Args:
@@ -132,15 +153,25 @@ def save_classified_image(classes, train_data_dir, file_name, classification):
     os.remove(file_name)
 
 
-# locate the training data dir and the file names
-train_data_dir, file_names = prepare_files(classes)
+def main() -> None:
+    # classes for which to make folders
+    classes = ["A1", "A3", "B1", "B3", "C1", "C2", "C3", "None"]
 
-# crop images
-image_crop_dict = {file_name: make_corners(
-    file_name, corners) for file_name in file_names}
+    # locate the training data dir and the file names
+    train_data_dir, file_names = prepare_files(classes)
 
-# prompt and save each image
-for file_name in image_crop_dict:
-    image_crops = image_crop_dict[file_name]
-    classification = prompt_for_class(os.path.basename(file_name), image_crops)
-    save_classified_image(classes, train_data_dir, file_name, classification)
+    # crop images
+    image_crop_dict = crop_images(file_names)
+
+    # prompt and save each image
+    for file_name in image_crop_dict:
+        image_crops = image_crop_dict[file_name]
+        classification = prompt_for_class(
+            os.path.basename(file_name), image_crops)
+        save_classified_image(classes, train_data_dir,
+                              file_name, classification)
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    main()
