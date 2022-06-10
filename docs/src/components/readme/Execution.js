9f7d4ba9d0a2f0f1b314440a3c44de6d0334b0a7
Martin Miglio <marmig0404@gmail.com>
2022-06-09 20:37:39 -0400

    fix typo

--- docs/src/components/readme/Execution.js
+++ docs/src/components/readme/Execution.js
@@ -13,7 +13,7 @@ export default class Execution extends Component {
       <div className='execution'>
         <h2>Run py-clash-bot</h2>
         <h4>a. Installed on Windows</h4>
-        <p>Run the d;esktop shortcut the installer created.</p>
+        <p>Run the desktop shortcut the installer created.</p>
         <h4>b. Installed with pip</h4>
         <p>Enter <code>pyclashbot</code> in the command line.</p>
       </div>
