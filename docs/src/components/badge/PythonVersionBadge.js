4e3631f310f607629ed363fd2e113ca72c39a539
Martin Miglio <marmig0404@gmail.com>
2022-06-22 13:13:08 -0400

    Add python version support badges

---
+++ docs/src/components/badge/PythonVersionBadge.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {pythonVersionBadgeEvent} from '../../GoogleAnalytics';
+
+/**
+* Component for latest release download link
+*/
+export default class PyPiBadge extends Component {
+  /**
+   * react render override
+   * @return {JSX.Element}
+   */
+  render() {
+    return (
+      <a href="https://www.python.org/downloads/" rel="nofollow" onClick={() => pythonVersionBadgeEvent()}>
+        <img
+          src="https://img.shields.io/pypi/pyversions/py-clash-bot"
+          alt="Python version"
+          style={{marginRight: '3px'}}
+          content="no-cache, no-store, must-revalidate"
+          rel="preconnect"
+          height="20"
+          width="84"
+        />
+      </a>
+    );
+  }
+}
