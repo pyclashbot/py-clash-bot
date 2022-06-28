ef60f4ace902b24db6c38be94cdd306aac0bf7af
Martin Miglio <marmig0404@gmail.com>
2022-06-28 18:34:03 -0400

    update compile testing

--- tests/compile_test.py
+++ tests/compile_test.py
@@ -4,9 +4,12 @@ from setuptools import sandbox
 
 
 class CompileTest(unittest.TestCase):
-    def test_windows_compile(self):
+    def test_msi_dist_compile(self):
         if system() == 'Windows':
             sandbox.run_setup('setup_msi.py', ['bdist_msi'])
 
-    def test_wheel_compile(self):
+    def test_wheel_dist_compile(self):
+        sandbox.run_setup('setup.py', ['bdist_wheel'])
+
+    def test_source_dist_compile(self):
         sandbox.run_setup('setup.py', ['sdist'])
