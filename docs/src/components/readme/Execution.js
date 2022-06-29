176c721103722e1054bb916b853d99db3cf3a744
Martin Miglio <marmig0404@gmail.com>
2022-06-29 12:11:44 -0400

    update how to run info

--- docs/src/components/readme/Execution.js
+++ docs/src/components/readme/Execution.js
@@ -12,6 +12,10 @@ export default class Execution extends Component {
     return (
       <div className='execution'>
         <h2>Run py-clash-bot</h2>
+        <p>
+          Before attempting to run the bot, make sure both the &#39;Multi
+          Instance Manager&#39; and the MEmu client are both open.
+        </p>
         <h4>a. Installed on Windows</h4>
         <p>Run the desktop shortcut the installer created.</p>
         <h4>b. Installed with pip</h4>
