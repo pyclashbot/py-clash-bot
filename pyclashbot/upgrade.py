b13176be1552381fc7956208f013f0faf44d96e7
Martin Miglio <marmig0404@gmail.com>
2022-10-21 20:18:20 -0400

    refactor + simplify

--- pyclashbot/upgrade.py
+++ pyclashbot/upgrade.py
@@ -17,21 +17,21 @@ from pyclashbot.client import show_image
 
 #Method for upgrading a given card
 def upgrade_card():
-    #Starts on the page of a card that you want to upgrade (after you 
+    #Starts on the page of a card that you want to upgrade (after you
     #click upgrade in the card list on the card page on clash main menu)
-    
+
     #Click upgrade for gold button
     click(238,606)
     time.sleep(1)
-    
+
     #Click second upgrade for gold button
     click(234,536)
     time.sleep(1)
-    
+
     #Click close to 'not enough gold' notification
     click(346,252)
     time.sleep(1)
-    
+
     #Click dead space to close card page
     for _ in range(5): click(26,518)
 
@@ -39,8 +39,8 @@ def upgrade_card():
 def upgrade_current_cards(logger):
     #Starts on the clash main card page looking at your main deck
     #Ends in the same spot
-    
-    
+
+
     #make list of coords of the 8 cards on display
     card_coord_list=[
         [86,278],
@@ -63,20 +63,20 @@ def upgrade_current_cards(logger):
         [281,465],
         [303,466],
     ]
-        
+
     for n in range(8):
         card_coord=card_coord_list[n]
         upgrade_button_coord=upgrade_button_coords[n]
-        
+
         click(card_coord[0],card_coord[1])
         time.sleep(1)
 
- 
+
         #check if upgrade button is there
         pix=numpy.asarray(screenshot())[upgrade_button_coord[1]+10][upgrade_button_coord[0]+10]
         #print(pix)
         if check_if_pixel_indicates_upgrade(pix):
-            logger.change_status(str("Upgrading card: "+str(n+1)))
+            logger.change_status(f"Upgrading card: {str(n + 1)}")
             click(upgrade_button_coord[0],upgrade_button_coord[1])
             time.sleep(1)
             upgrade_card()
@@ -87,16 +87,14 @@ def check_if_pixel_indicates_upgrade(pixel):
     positive_color_list=[
             [ 42, 172,  55],[ 96 ,217, 110],[ 65 ,224 , 80],[ 34 ,104 , 42]
     ]
-    for color in positive_color_list:
-        if pixel_is_equal(pixel,color,tol=30): return True
-    return False
+    return any(pixel_is_equal(pixel,color,tol=30) for color in positive_color_list)
 
 #Method to get to the clash royale main menu screen from the card page
 def get_to_clash_main_from_card_page(logger):
     logger.change_status("Getting to clash main from card page")
     click(250,630)
     loops=0
-    
+
     on_clash_main=check_if_on_clash_main_menu()
     while not(on_clash_main):
         loops+=1
@@ -171,36 +169,35 @@ def select_second_deck(logger):
     #get to card page
     get_to_card_page(logger)
     time.sleep(1)
-    
+
     #click number 2
     click(173,190)
     time.sleep(1)
-    
+
     #get to main menu from card page
     get_to_clash_main_from_card_page(logger)
-    
+
 #Method to randomize deck number 2 of this account
 def randomize_and_select_deck_2(logger):
     logger.change_status("Randomizing deck number 2")
     #get to card page
     get_to_card_page(logger)
-    
+
     #select deck 2
     click(173,190)
     time.sleep(1)
-    
+
     #click deck options tab
     click(339,165)
     time.sleep(1)
-    
+
     #click randomize button
     click(253,173)
     time.sleep(1)
-    
+
     #click confirm
     click(261,419)
     time.sleep(1)
-    
+
     #return to clash main
     get_to_clash_main_from_card_page(logger)
-    
