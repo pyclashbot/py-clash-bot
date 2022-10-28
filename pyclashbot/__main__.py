01120cb08fae861e8d00139d8ae48545db06f54c
Martin Miglio <marmig0404@gmail.com>
2022-10-28 00:40:12 -0400

    clean up thread after window close formatting

--- pyclashbot/__main__.py
+++ pyclashbot/__main__.py
@@ -9,8 +9,8 @@ from pyclashbot.states import detect_state, state_tree
 from pyclashbot.thread import StoppableThread
 
 
-# Method for reading the attributes of the window
 def read_window(window: sg.Window):
+    # Method for reading the attributes of the window
     read_result = window.read()
     if read_result is None:
         print("Window not found")
@@ -59,7 +59,7 @@ def stop_button_event(window, thread):
 def main_gui():
     # Method for the main gui that starts the program
     out_text = "Matthew Miglio ~October 2022\n\n-------------------------------------------------------------------------------------\nPy-ClashBot can farm gold, chest, and card\nprogress by farming 2v2 matches with random teammates.\n-------------------------------------------------------------------------------------"
-    sg.theme('Material2')
+    sg.theme("Material2")
     # defining various things that are gonna be in the gui.
     layout = [
         # first text lines
@@ -67,24 +67,35 @@ def main_gui():
         # first checkboxes
         [sg.Text("Select which jobs youd like the bot to do:")],
         [
-            sg.Checkbox('Open chests', default=False, key="-Open-Chests-in-"),
-            sg.Checkbox('Fight', default=False, key="-Fight-in-"),
-            sg.Checkbox('Random Requesting', default=False,
+            sg.Checkbox("Open chests", default=False, key="-Open-Chests-in-"),
+            sg.Checkbox("Fight", default=False, key="-Fight-in-"),
+            sg.Checkbox("Random Requesting", default=False,
                         key="-Requesting-in-"),
-            sg.Checkbox('Upgrade cards', default=False,
+            sg.Checkbox("Upgrade cards", default=False,
                         key="-Upgrade_cards-in-"),
-
         ],
         # dropdown for amount of accounts
-        [sg.Text("-------------------------------------------------------------------------------------\nChoose how many accounts you'd like to simultaneously farm:")],
-        [sg.Combo(['1', '2', '3', '4'], key='-SSID_IN-', default_value='1')],
-        [sg.Text("-------------------------------------------------------------------------------------")],
+        [
+            sg.Text(
+                "-------------------------------------------------------------------------------------\nChoose how many accounts you'd like to simultaneously farm:"
+            )
+        ],
+        [sg.Combo(["1", "2", "3", "4"], key="-SSID_IN-", default_value="1")],
+        [
+            sg.Text(
+                "-------------------------------------------------------------------------------------"
+            )
+        ],
         # bottons at bottom
-        [sg.Button('Start'), sg.Button('Stop', disabled=True),
-         sg.Button('Help'), sg.Button('Donate')],
-        [sg.Output(size=(88, 20), font=("Consolas 10"))]
+        [
+            sg.Button("Start"),
+            sg.Button("Stop", disabled=True),
+            sg.Button("Help"),
+            sg.Button("Donate"),
+        ],
+        [sg.Output(size=(88, 20), font=("Consolas 10"))],
     ]
-    window = sg.Window('Py-ClashBot', layout)
+    window = sg.Window("Py-ClashBot", layout)
 
     thread = None
     # run the gui
@@ -92,25 +103,29 @@ def main_gui():
         event, values = read_window(window)
 
         # if window close or exit button click
-        if event in [sg.WIN_CLOSED, 'Exit']:
+        if event in [sg.WIN_CLOSED, "Exit"]:
             break
 
         # If start button
-        if event == 'Start':
+        if event == "Start":
             thread = start_button_event(window, values)
 
-        elif event == 'Stop' and thread is not None:
+        elif event == "Stop" and thread is not None:
             stop_button_event(window, thread)
 
-        elif event == 'Donate':
+        elif event == "Donate":
             show_donate_gui()
 
-        elif event == 'Help':
+        elif event == "Help":
             show_help_gui()
 
     window.close()
 
-# main program
+    # shut down the thread if it is still running
+    if thread is not None:
+        thread.shutdown_flag.set()
+        # wait for the thread to close
+        thread.join()
 
 
 class MainLoopThread(StoppableThread):
@@ -118,26 +133,20 @@ class MainLoopThread(StoppableThread):
         super().__init__(args, kwargs)
 
     def run(self):
-        print(f"Thread #{self.ident} started")
-
         jobs, ssid_total = self.args
 
         logger = Logger()
         logger.log()
         ssid = 0
 
-        # Starting with restart state, the bot will pass itself between
-        # states as it reads the screen and will ignore jobs not on the joblist
+        # detect initial state
         state = detect_state(logger)
         while not self.shutdown_flag.is_set():
-            # perform a state decision based on the current state and get the
-            # next state
+            # enter state tree
             state = state_tree(jobs, logger, ssid, state)
-            # increment SSID to run the next loop with the next account in the
-            # cycle
+            # get next account ssid
             ssid = get_next_ssid(ssid, ssid_total)
 
-        print(f"Thread #{self.ident} stopped")
 
-
-main_gui()
+if __name__ == "__main__":
+    main_gui()
