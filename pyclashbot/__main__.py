5efcc86c966e373950b0545f8c4d02bc7759d026
Martin Miglio <marmig0404@gmail.com>
2022-11-02 17:34:58 -0400

    change restting of mainloopthread logger remove get next ssid function

--- pyclashbot/__main__.py
+++ pyclashbot/__main__.py
@@ -3,7 +3,6 @@ from typing import Union
 
 import PySimpleGUI as sg
 
-from pyclashbot.client import get_next_ssid
 from pyclashbot.gui import show_donate_gui, show_help_gui
 from pyclashbot.logger import Logger
 from pyclashbot.states import detect_state, state_tree
@@ -19,7 +18,7 @@ def read_window(window: sg.Window):
     return read_result
 
 
-def start_button_event(window, values):
+def start_button_event(logger:Logger, window, values):
     # get job list
     jobs = []
     if values["-Open-Chests-in-"]:
@@ -50,7 +49,7 @@ def start_button_event(window, values):
     window["Start"].update(disabled=True)
 
     # create thread
-    thread = MainLoopThread(args)
+    thread = MainLoopThread(logger, args)
     # start thread
     thread.start()
 
@@ -124,6 +123,7 @@ def main_gui():
     window = sg.Window("Py-ClashBot", layout)
 
     thread: Union[MainLoopThread, None] = None
+    logger = Logger()
     # run the gui
     while True:
         event, values = read_window(window)
@@ -134,10 +134,11 @@ def main_gui():
 
         # If start button
         if event == "Start":
-            thread = start_button_event(window, values)
+            thread = start_button_event(logger, window, values)
 
         elif event == "Stop" and thread is not None:
             stop_button_event(window, thread)
+            logger = Logger() # reset the logger after thread has been stopped
 
         elif event == "Donate":
             show_donate_gui()
@@ -155,9 +156,9 @@ def main_gui():
 
 
 class MainLoopThread(StoppableThread):
-    def __init__(self, args, kwargs=None):
+    def __init__(self, logger:Logger, args, kwargs=None):
         super().__init__(args, kwargs)
-        self.logger = Logger()
+        self.logger = logger
         self.logger.log()
 
     def run(self):
