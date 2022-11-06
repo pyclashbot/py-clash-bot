cda19b0a21a807b4db497ee2d035db0ca99892de
Martin Miglio <marmig0404@gmail.com>
2022-11-06 14:20:57 -0500

    Add error logging, catch all errors in worker thread and save to status Fix file logging to save in app data extract update log function to signal to log a new line etc pass error to status

--- pyclashbot/logger.py
+++ pyclashbot/logger.py
@@ -1,6 +1,11 @@
 import time
+from os.path import expandvars, join
 from queue import Queue
 from typing import Any, Union
+
+module_name = "py-clash-bot"
+
+top_level = join(expandvars("%appdata%"), module_name)
 
 
 class Logger:
@@ -12,16 +17,25 @@ class Logger:
         file_log (bool, optional): Enable file logging. Defaults to True.
     """
 
-    def __init__(self,queue: Union[Queue, None] = None,console_log: bool = False,file_log: bool = True,
+    def __init__(
+        self,
+        queue: Union[Queue, None] = None,
+        console_log: bool = False,
+        file_log: bool = True,
     ):
 
-        # track enabled log types
+        # setup console log
         self.console_log = console_log
-        self.file_log = file_log
+        # print buffer for console
+        self.console_buffer = ""
 
+        # setup log file
+        self.file_log = file_log
+        # print buffer for file
+        self.file_buffer = ""
         # open log file
         if file_log:
-            self._log_file = open("log.txt", "w")
+            self._log_file = open(join(top_level, "log.txt"), "w")
 
         # queue for threaded communication
         self._queue = Queue() if queue is None else queue
@@ -45,15 +59,19 @@ class Logger:
         self.war_battles_fought = 0
         self.current_status = "Idle"
 
-        # print buffer for console
-        self.console_buffer = ""
-
         # write initial values to queue
         self._update_queue()
 
     def __del__(self):
         if self.file_log:
             self._log_file.close()
+
+    def _update_log(self):
+        self._update_queue()
+        if self.console_log:
+            self.log_to_console()
+        if self.file_log:
+            self.log_to_file()
 
     def _update_queue(self):
         """updates the queue with a dictionary of mutable statistics"""
@@ -77,6 +95,139 @@ class Logger:
             "current_status": self.current_status,
         }
         self._queue.put(statistics)
+
+    def log_to_console(self):
+        """log to console"""
+
+        line_with_leftside = "|----------------------------------------------------------------------------------"
+        line = "-----------------------------------------------------------------------------------"
+
+        self.console_add_row(line)
+        self.console_add_row(
+            f"|           Program uptime:           | {self.make_timestamp()}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(f"|          Program restarts:          | {self.restarts}")
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(f"|              Requests:              | {self.requests}")
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(f"|               Fights:               | {self.fights}")
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|              Win rate:              | {self.make_win_loss_str()}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|           Chests unlocked:          | {self.chests_unlocked}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|            Cards played:            | {self.cards_played}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|            Cards upgraded:          | {self.cards_upgraded}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|           Account switches:         | {self.account_switches}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|   Card Mastery Reward Collections   | {self.card_mastery_reward_collections}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|    Battlepass Reward Collections    | {self.battlepass_rewards_collections}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|     Level Up Reward Collections     | {self.level_up_chest_collections}"
+        )
+        self.console_add_row(line_with_leftside)
+        self.console_add_row(
+            f"|          War Battles Fought         | {self.war_battles_fought}"
+        )
+        self.console_add_row(line_with_leftside)
+
+        self.console_add_row(f"|      Current status:      | {self.current_status}")
+        self.console_add_row(line)
+        self.print_buffer()
+
+    def log_to_file(self):
+        """log to file"""
+
+        self.file_add_row(f"{self.make_timestamp()} | {self.current_status}")
+
+        # log to file
+        self._log_file.write(self.file_buffer)
+
+        # clear buffer
+        self.file_buffer = ""
+
+    def console_add_row(self, row: str) -> None:
+        """add row to log
+
+        Args:
+            row (str): row to add to log
+        """
+        if row is not None:
+            row = self.line_wrap(row, 85)
+            self.console_buffer += row + "\n"
+
+    def file_add_row(self, row: str) -> None:
+        """add row to log
+
+        Args:
+            row (str): row to add to log
+        """
+        if row is not None:
+            self.file_buffer += row + "\n"
+
+    def print_buffer(self):
+        """print log buffer"""
+        print(self.console_buffer)
+        self.console_buffer = ""  # clear buffer
+
+    def error(self, message: str):
+        """log error message
+
+        Args:
+            message (str): error message
+        """
+        self.change_status(f"ERROR: {message}")
+        self._update_log()
+
+    def line_wrap(self, line: str, width: int) -> str:
+        """wrap line to width
+
+        Args:
+            line (str): line to wrap
+            width (int): width to wrap line to
+
+        Returns:
+            str: wrapped line
+        """
+
+        new_line = "\n|                           | "
+
+        # split line into words
+        words = line.split(" ")
+
+        # wrap line
+        wrapped_line = ""
+        current_line = ""
+        for word in words:
+            if len(current_line) + len(word) < width:
+                current_line += f"{word} "
+            else:
+                wrapped_line += current_line + new_line
+                current_line = f"{word} "
+
+        # add last line
+        wrapped_line += current_line
+
+        return wrapped_line
 
     def make_timestamp(self):
         """creates a time stamp for log output
@@ -115,198 +266,69 @@ class Logger:
         seconds %= 60
         return "%d:%02d:%02d" % (hour, minutes, seconds)
 
-    def log_to_console(self):
-        """log to console"""
-
-        line_with_leftside = "|----------------------------------------------------------------------------------"
-        line = "-----------------------------------------------------------------------------------"
-
-        self.add_row(line)
-        self.add_row(f"|           Program uptime:           | {self.make_timestamp()}")
-        self.add_row(line_with_leftside)
-        self.add_row(f"|          Program restarts:          | {self.restarts}")
-        self.add_row(line_with_leftside)
-        self.add_row(f"|              Requests:              | {self.requests}")
-        self.add_row(line_with_leftside)
-        self.add_row(f"|               Fights:               | {self.fights}")
-        self.add_row(line_with_leftside)
-        self.add_row(
-            f"|              Win rate:              | {self.make_win_loss_str()}"
-        )
-        self.add_row(line_with_leftside)
-        self.add_row(f"|           Chests unlocked:          | {self.chests_unlocked}")
-        self.add_row(line_with_leftside)
-        self.add_row(f"|            Cards played:            | {self.cards_played}")
-        self.add_row(line_with_leftside)
-        self.add_row(f"|            Cards upgraded:          | {self.cards_upgraded}")
-        self.add_row(line_with_leftside)
-        self.add_row(f"|           Account switches:         | {self.account_switches}")
-        self.add_row(line_with_leftside)
-        self.add_row(
-            f"|   Card Mastery Reward Collections   | {self.card_mastery_reward_collections}"
-        )
-        self.add_row(line_with_leftside)
-        self.add_row(
-            f"|    Battlepass Reward Collections    | {self.battlepass_rewards_collections}"
-        )
-        self.add_row(line_with_leftside)
-        self.add_row(
-            f"|     Level Up Reward Collections     | {self.level_up_chest_collections}"
-        )
-        self.add_row(line_with_leftside)
-        self.add_row(
-            f"|          War Battles Fought         | {self.war_battles_fought}"
-        )
-        self.add_row(line_with_leftside)
-
-        self.add_row(f"|      Current status:      | {self.current_status}")
-        self.add_row(line)
-        self.print_buffer()
-
-    def log_to_file(self):
-        """log to file"""
-
-        # log to file
-        self._log_file.write(self.console_buffer)
-
-        # clear buffer
-        self.console_buffer = ""
-
-    def line_wrap(self, line: str, width: int) -> str:
-        """wrap line to width
-
-        Args:
-            line (str): line to wrap
-            width (int): width to wrap line to
-
-        Returns:
-            str: wrapped line
-        """
-
-        new_line = "\n|                           | "
-
-        # split line into words
-        words = line.split(" ")
-
-        # wrap line
-        wrapped_line = ""
-        current_line = ""
-        for word in words:
-            if len(current_line) + len(word) < width:
-                current_line += f"{word} "
-            else:
-                wrapped_line += current_line + new_line
-                current_line = f"{word} "
-
-        # add last line
-        wrapped_line += current_line
-
-        return wrapped_line
-
-    def add_row(self, row: str) -> None:
-        """add row to log
-
-        Args:
-            row (str): row to add to log
-        """
-        if row is not None:
-            row = self.line_wrap(row, 85)
-            self.console_buffer += row + "\n"
-
-    def print_buffer(self):
-        """print log buffer"""
-        print(self.console_buffer)
-        self.console_buffer = ""  # clear buffer
-
     def add_battlepass_rewards_collection(self):
         """add battlepass rewards collection to log"""
         self.battlepass_rewards_collections += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_level_up_chest_collection(self):
         """add level up chest collection to log"""
         self.level_up_chest_collections += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_war_battle_fought(self):
         """add war battle fought to log"""
         self.war_battles_fought += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_card_mastery_reward_collection(self):
         self.card_mastery_reward_collections = self.card_mastery_reward_collections + 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_chest_unlocked(self):
         """add chest unlocked to log"""
         self.chests_unlocked += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_card_played(self):
         """add card played to log"""
         self.cards_played += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_card_upgraded(self):
         """add card upgraded to log"""
         self.cards_upgraded += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_account_switch(self):
         """add account switch to log"""
         self.account_switches += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_win(self):
         """add win to log"""
         self.wins += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_loss(self):
         """add loss to log"""
         self.losses += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_fight(self):
         """add fight to log"""
         self.fights += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_request(self):
         """add request to log"""
         self.requests += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def add_restart(self):
         """add restart to log"""
         self.restarts += 1
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
 
     def change_status(self, status):
         """change status of bot in log
@@ -315,6 +337,4 @@ class Logger:
             status (str): status of bot
         """
         self.current_status = status
-        if self.console_log:
-            self.log_to_console()
-        self._update_queue()
+        self._update_log()
