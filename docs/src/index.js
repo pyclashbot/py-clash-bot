b5b4d41df9064df9e027fbfbc47e3c6ecdea3d6e
Martin Miglio <marmig0404@gmail.com>
2022-11-07 16:48:45 -0500

    fix eslinting

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
 
 reportWebVitals();
