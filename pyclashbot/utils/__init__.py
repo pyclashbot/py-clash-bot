c1ab892de417fcb4ad1913bd877769f867a1238a
Martin Miglio <marmig0404@gmail.com>
2022-11-07 12:19:29 -0500

    only export dependency if on windows

--- pyclashbot/utils/__init__.py
+++ pyclashbot/utils/__init__.py
@@ -1,17 +1,21 @@
+import contextlib
+
 from .caching import (_cache_data, _load_data, cache_user_settings,
                       check_user_settings, read_user_settings)
-from .dependency import setup_ahk, setup_memu
 from .logger import Logger
 from .thread import StoppableThread
 
 __all__ = [
     "Logger",
     "StoppableThread",
-    "setup_memu",
-    "setup_ahk",
     "read_user_settings",
     "cache_user_settings",
     "check_user_settings",
     "_cache_data",
     "_load_data",
 ]
+
+# dependency module is only available on Windows
+with contextlib.suppress(ImportError):
+    from .dependency import setup_ahk, setup_memu
+    __all__.extend(["setup_ahk", "setup_memu"])
