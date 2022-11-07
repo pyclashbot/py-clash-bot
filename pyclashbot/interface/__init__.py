c870adeba61425317f5afd1a33c53175983bb625
Martin Miglio <marmig0404@gmail.com>
2022-11-07 14:01:24 -0500

    change reference to interface

--- pyclashbot/interface/__init__.py
+++ pyclashbot/interface/__init__.py
@@ -1,8 +1,15 @@
-from .layout import disable_keys, layout, show_help_gui, user_config_keys
+from .layout import (
+    disable_keys,
+    layout,
+    show_clash_royale_setup_gui,
+    show_help_gui,
+    user_config_keys,
+)
 
 __all__ = [
     "layout",
     "show_help_gui",
     "disable_keys",
     "user_config_keys",
+    "show_clash_royale_setup_gui",
 ]
