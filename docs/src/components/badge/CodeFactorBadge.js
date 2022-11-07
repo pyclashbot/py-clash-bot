7f9433f7a0cdb68dcdc96c140ba31c1475317420
Martin Miglio <marmig0404@gmail.com>
2022-11-07 14:31:15 -0500

    run pre-commit

--- docs/src/components/badge/CodeFactorBadge.js
+++ docs/src/components/badge/CodeFactorBadge.js
@@ -1,19 +1,23 @@
 // <a href="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot"><img src="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot/badge" alt="CodeFactor" /></a>
 
-import React, {Component} from 'react';
-import {codeFactorBadgeEvent} from '../../GoogleAnalytics';
+import React, { Component } from "react";
+import { codeFactorBadgeEvent } from "../../GoogleAnalytics";
 
 /**
-* Component for latest release download link
-*/
+ * Component for latest release download link
+ */
 export default class CodeFactorBadge extends Component {
   /**
-     * react render override
-     * @return {JSX.Element}
-     */
+   * react render override
+   * @return {JSX.Element}
+   */
   render() {
     return (
-      <a href="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot" rel="nofollow" onClick={() => codeFactorBadgeEvent()}>
+      <a
+        href="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot"
+        rel="nofollow"
+        onClick={() => codeFactorBadgeEvent()}
+      >
         <img
           src="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot/badge"
           alt="CodeFactor badge"
