db79dd95bd20b036ee3e823b16ff2d277ae1602c
Martin Miglio <marmig0404@gmail.com>
2022-11-09 13:59:09 -0500

    add isort to precommit add black and isort confi to pyproject

--- pyclashbot/memu/__init__.py
+++ pyclashbot/memu/__init__.py
@@ -2,14 +2,13 @@ from .client import (
     click,
     get_file_count,
     make_reference_image_list,
+    orientate_terminal,
     screenshot,
     scroll_down,
     scroll_down_super_fast,
     scroll_up_fast,
     scroll_up_super_fast,
-    orientate_terminal,
 )
-
 from .launcher import restart_and_open_clash
 
 __all__ = [
