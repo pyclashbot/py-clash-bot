a940268a3e59b49e20470221c4f67f669ac4e50f
Matthew Stream-PC <matmigg080@gmail.com>
2022-12-03 16:32:44 -0500

    commenting + fixing flow of free_offer_collection()

--- pyclashbot/bot/free_offer_collection.py
+++ pyclashbot/bot/free_offer_collection.py
@@ -21,17 +21,18 @@ def collect_free_offer_from_shop(logger):
     logger.change_status("Collecting free offer from shop.")
 
     if not check_if_on_clash_main_menu():
-        print("not on clash main so cant run collect_free_offer_from_shop()")
         print("Cant run collect_free_offer_from_shop() because not on main.")
         return "restart"
 
     # get to shop
+    print("getting to shop from main")
     get_to_shop_page_from_main(logger)
 
     # scroll a few times, each time looking for the free offer
     logger.change_status("Looking for free offer")
     free_offer_coords = None
     loops = 0
+    print("Scrolling and looking for a free offer")
     while free_offer_coords is None:
         scroll_down_super_fast()
         time.sleep(2)
@@ -40,11 +41,13 @@ def collect_free_offer_from_shop(logger):
 
         loops += 1
         if loops > 10:
+            print("looped looking for free offer too many times.")
             break
 
     if free_offer_coords is None:
         logger.change_status("Failed to find free offer icon.")
         # get to clash main from shop
+        print("Getting to clash main then returning.")
         if get_to_clash_main_from_shop(logger) == "restart":
             print(
                 "failed to get to clash main from shop page in collect_free_offer_from_shop()"
@@ -53,16 +56,18 @@ def collect_free_offer_from_shop(logger):
         return
 
     # click free offer
-    logger.change_status("Found free offer icon. Collecting it.")
+    logger.change_status("Found free offer. Clicking this free offer.")
     click(free_offer_coords[0], free_offer_coords[1])
     time.sleep(2)
+
+    print("Clicking on the 'free' collect button in the next screen")
     if click_find_free_button_in_shop() != "fail":
         # implement logging for this later
         logger.add_free_offer_collection()
 
     # click deadspace
-    for _ in range(4):
-        click(18, 379)
+    print("Clicking deadspace to skip through free reward")
+    click(20, 380, clicks=10, interval=0.1)
 
     # get to clash main from shop
     if get_to_clash_main_from_shop(logger) == "restart":
