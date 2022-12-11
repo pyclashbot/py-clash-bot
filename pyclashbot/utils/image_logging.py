6a67ef490be8e3c2458b5fe6d08de7712cfc9434
Matthew Miglio <matmigg0804@gmail.com>
2022-12-11 14:46:18 -0500

    image logging stuff

--- pyclashbot/utils/image_logging.py
+++ pyclashbot/utils/image_logging.py
@@ -1,8 +1,9 @@
 import os
 import time
 
-from pyscreeze import screenshot
+from pyclashbot.memu.client import screenshot
 
+MAX_FILE_COUNT = 100
 
 # method to get this file location
 def get_image_log_directory():
@@ -23,6 +24,45 @@ def save_image(image, name):
     image.save(directory + "\\" + name + ".png")
 
 
-# method to save an image to the image log
-def save_image_to_image_log(logger):
-    save_image(image=screenshot(), name=logger.calc_time_since_start())
+# method to parse the name of the image so it can be saved
+def parse_name_for_image(name):
+    return_string = ""
+    for char in name:
+        if char != ":":
+            return_string = return_string + char
+        else:
+            return_string = return_string + "_"
+    return return_string
+
+
+# method to save the current screen image to the log
+def save_this_screen_image_to_log(logger):
+    name = logger.calc_time_since_start()
+    name = parse_name_for_image(name)
+    print("saving image with name ", name)
+    save_image(image=screenshot(), name=name)
+    if count_files_in_directory() > MAX_FILE_COUNT:
+        delete_oldest_file_in_directory()
+
+
+# method to delete the oldest file in a directory
+def delete_oldest_file_in_directory():
+    directory = get_image_log_directory()
+    oldest_file = None
+    oldest_file_time = None
+    for file in os.listdir(directory):
+        file_time = os.path.getmtime(directory + "\\" + file)
+        if oldest_file_time is None or file_time < oldest_file_time:
+            oldest_file = file
+            oldest_file_time = file_time
+    print("Removing ", oldest_file)
+    os.remove(directory + "\\" + oldest_file)
+
+
+# method to count the amount of files in a directory
+def count_files_in_directory():
+    directory = get_image_log_directory()
+    count = 0
+    for _ in os.listdir(directory):
+        count = count + 1
+    return count
