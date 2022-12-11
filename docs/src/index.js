505c492a5fdc6d8de20ab90e8019f744b6c3596b
Martin Miglio <marmig0404@gmail.com>
2022-12-11 18:47:53 -0500

    update the website to match readme

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
