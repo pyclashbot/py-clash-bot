d6ed14979faf8d79da6ffcd675614a9fc62d5876
Martin Miglio <marmig0404@gmail.com>
2022-12-13 01:19:25 -0500

    add msi icon

--- setup_msi.py
+++ setup_msi.py
@@ -31,6 +31,7 @@ exe = Executable(
     shortcut_dir="DesktopFolder",
     target_name=f"{product_name}.exe",
     copyright="2022 Matthew Miglio",
+    icon="docs\\src\\assets\\pixel-pycb.ico",
 )
 
 setup(
