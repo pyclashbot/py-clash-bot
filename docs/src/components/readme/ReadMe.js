d74b68a15138edf5c58480afa65915c499bfd148
Martin Miglio <marmig0404@gmail.com>
2022-12-13 01:20:52 -0500

    update website style add logo use button for release download

--- docs/src/components/readme/ReadMe.js
+++ docs/src/components/readme/ReadMe.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import {handleClick} from '../../GoogleAnalytics';
 import ReleaseLink from '../release/ReleaseLink';
+import logo from '../../assets/pixel-pycb.svg';
 import './ReadMe.css';
 
 /**
@@ -16,35 +17,43 @@ export default class ReadMe extends Component {
     return (
       <div className="ReadMe">
         <div className='block'>
-          <h1>py-clash-bot</h1>
-          <h2 className='badge-flex'>
-            <a className='badge' href="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-build-msi.yml" onClick={handleClick}>
-              <img
-                alt="Build windows package"
-                src="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-build-msi.yml/badge.svg"
-                content="no-cache, no-store, must-revalidate"
-              />
-            </a>
-            <a className='badge' href="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml" onClick={handleClick}>
-              <img
-                alt="GitHub Python Tests"
-                src="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml/badge.svg"
-                content="no-cache, no-store, must-revalidate"
-              />
-            </a>
-            <a className='badge' href="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot" onClick={handleClick}>
-              <img
-                alt="CodeFactor"
-                src="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot/badge"
-                content="no-cache, no-store, must-revalidate"
-              />
-            </a>
-          </h2>
+          <div className='header-block'>
+            <div className='text-block'>
+              <h1>py-clash-bot</h1>
+              <h2 className='badge-flex'>
+                <a className='badge' href="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-build-msi.yml" onClick={handleClick}>
+                  <img
+                    alt="Build windows package"
+                    src="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-build-msi.yml/badge.svg"
+                    content="no-cache, no-store, must-revalidate"
+                  />
+                </a>
+                <a className='badge' href="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml" onClick={handleClick}>
+                  <img
+                    alt="GitHub Python Tests"
+                    src="https://github.com/matthewmiglio/py-clash-bot/actions/workflows/python-tests.yml/badge.svg"
+                    content="no-cache, no-store, must-revalidate"
+                  />
+                </a>
+                <a className='badge' href="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot" onClick={handleClick}>
+                  <img
+                    alt="CodeFactor"
+                    src="https://www.codefactor.io/repository/github/matthewmiglio/py-clash-bot/badge"
+                    content="no-cache, no-store, must-revalidate"
+                  />
+                </a>
+              </h2>
+            </div>
+            <div className='logo-block'>
+              <img className='logo' alt="py-clash-bot logo" src={logo}
+                content="max-age=259200" />
+            </div>
+          </div>
           <p>
-          py-clash-bot is an open-source application that allows users to automate
-          their Clash Royale gameplay on Windows using an emulated Android phone. The
-          bot uses a combination of image recognition, mouse control, and Android
-          emulation to perform a variety of tasks without user intervention.
+                py-clash-bot is an open-source application that allows users to automate
+                their Clash Royale gameplay on Windows using an emulated Android phone. The
+                bot uses a combination of image recognition, mouse control, and Android
+                emulation to perform a variety of tasks without user intervention.
           </p>
         </div>
         <div className='block'>
@@ -59,7 +68,7 @@ export default class ReadMe extends Component {
           <h2>Installation</h2>
           <ol>
             <li>
-              <ReleaseLink child="Download and run the latest windows installer"/>
+              <ReleaseLink child={<button className='download-button'>Download</button>} /> and run the latest windows installer
             </li>
             <li>Run the shortcut on the desktop</li>
             <li>
