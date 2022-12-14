d5b52b3b3bf3e9b06d5a31c3312fe90c0e6b03fa
Martin Miglio <marmig0404@gmail.com>
2022-12-14 17:51:16 -0500

    add debug mode for g4a tracking
    Former-commit-id: dd4a9586ca2d116ecb2d1cc98341ca0607a5cc5b

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,5 +1,4 @@
 import ReadMe from './components/readme/ReadMe';
-import {initializeGA} from './GoogleAnalytics';
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import AppHelmet from './components/AppHelmet';
@@ -10,7 +9,6 @@ import './App.css';
  * @return {Component} App
  */
 export default function App() {
-  initializeGA();
   return (
     <Router basename="/py-clash-bot">
       <div className="App">
