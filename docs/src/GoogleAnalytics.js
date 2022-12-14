30ff32e780fcd3bf1eb4bce957894da2cb5bd507
Martin Miglio <marmig0404@gmail.com>
2022-12-14 17:51:16 -0500

    add debug mode for g4a tracking
    Former-commit-id: c812051be4e7ed216b53d50644e3046b9eee654b

--- docs/src/GoogleAnalytics.js
+++ docs/src/GoogleAnalytics.js
@@ -1,4 +1,5 @@
 import ReactGA from 'react-ga4';
+import {process} from 'process';
 
 const GA_MEASUREMENT_ID = 'G-Y7520HNKMG';
 
@@ -6,37 +7,25 @@ const GA_MEASUREMENT_ID = 'G-Y7520HNKMG';
  * Initialize google analytics with tracking ID and settings
  */
 export function initializeGA() {
-  ReactGA.initialize(GA_MEASUREMENT_ID);
+  const isDev = process.env.NODE_ENV === 'development';
+  console.log('isDev: ', isDev);
+  ReactGA.initialize(GA_MEASUREMENT_ID, {
+    gaOptions: {debug_mode: isDev},
+    gtagOptions: {debug_mode: isDev},
+    debug: isDev,
+  });
 }
+
 
 /**
  * updates reactga with a page change
  * @param {string} page The page to update ReactGA with.
  */
 export function pageChange(page) {
-  const devQuery = '?utm_source=dev&utm_medium=dev';
-  if (window.location.href.split('/').pop() == devQuery) {
-    ReactGA.initialize(GA_MEASUREMENT_ID, {
-      gaOptions: {
-        debug_mode: true,
-      },
-      gtagOptions: {
-        debug_mode: true,
-      },
-    });
-  }
   ReactGA.send({
     hitType: 'pageview',
     page: window.location.pathname + window.location.search,
   });
-
-  if (
-    window.location.href.split('/').pop() !== devQuery &&
-    // eslint-disable-next-line no-undef
-    (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')
-  ) {
-    window.location.href = page + devQuery;
-  }
 }
 
 // Track link clicks
@@ -48,10 +37,10 @@ export const handleClick = (event) => {
   });
 };
 
-export const handleDownload = (event) => {
+export const handleDownload = (event, downloadLink) => {
   ReactGA.event({
     category: 'Release',
     action: 'Download',
-    label: event.currentTarget.href,
+    label: downloadLink,
   });
 };
