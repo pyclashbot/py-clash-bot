a394197c4f7345f8e5425113e022ff25fae6faa0
Martin Miglio <marmig0404@gmail.com>
2022-12-14 17:13:23 -0500

    set debug mode for utm source dev
    Former-commit-id: 221fbf26fd9c35161721a6c7510c39a62797c459

--- docs/src/GoogleAnalytics.js
+++ docs/src/GoogleAnalytics.js
@@ -1,10 +1,12 @@
 import ReactGA from 'react-ga4';
 
+const GA_MEASUREMENT_ID = 'G-Y7520HNKMG';
+
 /**
  * Initialize google analytics with tracking ID and settings
  */
 export function initializeGA() {
-  ReactGA.initialize('G-Y7520HNKMG');
+  ReactGA.initialize(GA_MEASUREMENT_ID);
 }
 
 /**
@@ -12,11 +14,22 @@ export function initializeGA() {
  * @param {string} page The page to update ReactGA with.
  */
 export function pageChange(page) {
+  const devQuery = '?utm_source=dev&utm_medium=dev';
+  if (window.location.href.split('/').pop() == devQuery) {
+    ReactGA.initialize(GA_MEASUREMENT_ID, {
+      gaOptions: {
+        debug_mode: true,
+      },
+      gtagOptions: {
+        debug_mode: true,
+      },
+    });
+  }
   ReactGA.send({
     hitType: 'pageview',
     page: window.location.pathname + window.location.search,
   });
-  const devQuery = '?utm_source=dev&utm_medium=dev';
+
   if (
     window.location.href.split('/').pop() !== devQuery &&
     // eslint-disable-next-line no-undef
