106a7aeb02bca891ee4c51997e35ca963842e7d2
Matthew Miglio <matmigg0804@gmail.com>
2022-12-15 17:00:59 -0500

    implemented navigation.py and admin_check.py
    Former-commit-id: 5ea6ef157029dfc8c39d1c3adf6dc6f1e1581cd8

--- pyclashbot/bot/free_offer_collection.py
+++ pyclashbot/bot/free_offer_collection.py
@@ -3,6 +3,10 @@ import time
 import numpy
 
 from pyclashbot.bot.clashmain import check_if_on_clash_main_menu
+from pyclashbot.bot.navigation import (
+    get_to_clash_main_from_shop,
+    get_to_shop_page_from_main,
+)
 from pyclashbot.detection.image_rec import (
     find_references,
     get_first_location,
@@ -126,79 +130,3 @@ def find_free_offer_icon():
 
     coord = get_first_location(locations)
     return None if coord is None else [coord[1], coord[0]]
-
-
-def get_to_clash_main_from_shop(logger):
-    logger.change_status("Getting to clash main from shop.")
-    # click main
-    click(240, 630)
-    time.sleep(2)
-
-    loops = 0
-    while not check_if_on_clash_main_menu():
-        loops += 1
-        if loops > 20:
-            print(
-                "Looped through get_to_clash_main_from_shop() too many times. Restarting"
-            )
-            return "restart"
-        click(200, 620)
-        time.sleep(2)
-
-    logger.change_status("Made it to clash main from shop.")
-
-
-def get_to_shop_page_from_main(logger):
-    logger.change_status("Getting to shop from main.")
-
-    # check if on main
-    if not check_if_on_clash_main_menu():
-        logger.change_status(
-            "not no clash main so cant run get_to_shop_page_from_main()"
-        )
-        return "restart"
-
-    # click shop icon
-    click(35, 636)
-    time.sleep(1)
-
-    # check if on shop
-    if not check_if_on_shop_page_with_delay():
-        logger.change_status("Failed to get to shop page.")
-        return "restart"
-
-    logger.change_status("Made it to shop from main.")
-
-
-def check_if_on_shop_page():
-    iar = numpy.asarray(screenshot())
-
-    pix_list = [
-        iar[624][71],
-        iar[636][55],
-        iar[623][85],
-        iar[625][71],
-    ]
-    color_list = [
-        [103, 234, 56],
-        [56, 85, 101],
-        [247, 194, 75],
-        [103, 236, 56],
-    ]
-    for n in range(4):
-        this_pixel = pix_list[n]
-        this_color = color_list[n]
-        if not pixel_is_equal(this_pixel, this_color, tol=35):
-            return False
-    return True
-
-
-def check_if_on_shop_page_with_delay():
-    start_time = time.time()
-    while time.time() - start_time < 3:
-        print("looping thru")
-        if check_if_on_shop_page():
-            print("made it")
-            return True
-        time.sleep(0.5)
-    return False
