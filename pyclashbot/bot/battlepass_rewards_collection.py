3a83b1ec4ecbcad38610e9aa66463bbdd087d123
Matthew Stream-PC <matmigg080@gmail.com>
2022-12-20 18:21:23 -0500

    sped up battlepass collection + open_chests.py
    Former-commit-id: 7ee8dc9625587f9620c72bf48fba031dc9f3a01a

--- pyclashbot/bot/battlepass_rewards_collection.py
+++ pyclashbot/bot/battlepass_rewards_collection.py
@@ -74,17 +74,14 @@ def collect_battlepass_rewards(logger):
         get_to_battlepass_rewards_page()
 
         # click every chest locations in the chest_locations list
-        for coord in chest_locations:
-            click(coord[0], coord[1], duration=0.1)
-            time.sleep(0.33)
+        for coord in chest_locations:click(coord[0], coord[1], duration=0.1)
 
         # click deadspace
-        for _ in range(15):
-            click(20, 440, duration=0.1)
-            time.sleep(0.1)
+        click(20, 440, duration=0.1,clicks=15,interval=0.33)
 
         # close battlepass to reset UI and return to clash main
         click(210, 630)
+       
         if wait_for_clash_main_menu(logger)=="restart":
             print("waited too long for clash main menu to return after closing battlepass")
             return "restart"
@@ -93,7 +90,6 @@ def collect_battlepass_rewards(logger):
         logger.add_battlepass_reward_collection()
 
     logger.change_status("Done collecting battlepass rewards.")
-    time.sleep(1)
 
     # should be on clash main at this point
     if check_if_on_clash_main_menu():
