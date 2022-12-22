47eb05d1d6e9ad42b9e98147ac0d50ef7edee56f
Martin Miglio <marmig0404@gmail.com>
2022-12-22 02:22:23 -0500

    add backend server, update build scripts and deps
    dont show popup for clash setup
    
    use with for mutex use
    
    add single exe setup script for backend
    
    move pysimplegui to dev deps
    
    include msvcr in backend dist
    
    move old main to debug_gui
    
    deprecate msi build
    
    update compile test to run setup_exe
    
    ignore folders python linting

---
+++ backend/pyclashbot/server.py
@@ -0,0 +1,95 @@
+import signal
+
+from flask import Flask, request
+from flask_cors import CORS
+from gevent.pywsgi import WSGIServer
+
+from pyclashbot.bot import WorkerThread
+from pyclashbot.utils.logger import Logger
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+# Create global variable for thread
+thread: WorkerThread | None = None
+logger = Logger()
+
+
+# Define route to start thread
+@app.route("/start-thread", methods=["POST"])
+def start_thread():
+    try:
+        global thread
+        if request.json is not None and thread is None:
+            selected_jobs: list = request.json["selectedJobs"]
+            selected_accounts: int = request.json["selectedAccounts"]
+            args = (selected_jobs, selected_accounts)
+            thread = WorkerThread(logger, args)
+            thread.start()
+
+        # Return success response
+        return {"status": "started", "message": "Thread started"}
+    except Exception as err:
+        return {"status": "failed", "message": f"Error: {err}"}
+
+
+# Define route to stop thread
+@app.route("/stop-thread", methods=["GET"])
+def stop_thread():
+    # Stop thread
+    global thread, logger
+    if thread is not None:
+        thread.shutdown()
+        thread = None
+        logger = Logger()
+
+    # Return success response
+    return {"status": "stopped", "message": "Thread stopped"}
+
+
+@app.route("/output")
+def handle_output():
+    # read the value of the mutex output on the thread object
+    stats = logger.get_stats()
+    if thread is not None and stats is not None:
+        if logger.errored:
+            return {"status": "errored", "message": f"Error: {stats['current_status']}"}
+        return {
+            "status": "running",
+            "message": f"{stats['current_status']}",
+            "statistics": stats,
+        }
+    return {"status": "stopped", "message": "No thread running", "statistics": {}}
+
+
+<<<<<<< HEAD
+=======
+@app.route("/heartbeat")
+def heartbeat():
+    return {"status": "listening", "message": "Server running"}
+
+
+class SigTermException(Exception):
+    pass
+
+
+def sigterm_handler(_signo, _stack_frame):
+    # Raises SystemExit(0):
+    raise SigTermException
+
+
+>>>>>>> 5316ffd (adding prints to server.py)
+http_server = WSGIServer(("127.0.0.1", 1357), app)
+
+try:
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    http_server.serve_forever()
+except (KeyboardInterrupt, SigTermException):
+    print("Shutting down server")
+    http_server.stop()
+    if thread is not None:
+        thread.shutdown()
+        thread = None
+        logger = Logger()
+    print("Server shut down")
+    exit(0)
---
+++ backend/setup_exe.py
@@ -0,0 +1,40 @@
+import subprocess
+
+from cx_Freeze import Executable, setup
+
+PRODUCT_NAME = "pycb"
+
+try:
+    VERSION = str(
+        subprocess.check_output(
+            ["git", "describe", "--abbrev=0"], stderr=subprocess.STDOUT
+        )
+    ).strip("'b\\n")
+except subprocess.CalledProcessError:
+    VERSION = "0.0.0"
+
+build_exe_options = {
+    "include_msvcr": True,
+    "excludes": [
+        "PySimpleGUI",
+        "tkinter",
+        "test",
+        "setuptools",
+        "pyclashbot.interface",
+        "matplotlib",
+    ],
+}
+
+exe = Executable(
+    script="pyclashbot\\server.py",
+    base=None,
+    target_name=f"{PRODUCT_NAME}_back.exe",
+    uac_admin=True,
+)
+
+setup(
+    name=PRODUCT_NAME,
+    version=VERSION,
+    executables=[exe],
+    options={"build_exe": build_exe_options},
+)
