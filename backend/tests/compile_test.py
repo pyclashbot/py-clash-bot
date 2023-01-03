9a86f9154847c4418dfb92a7d140117467f8818f
Martin Miglio <marmig0404@gmail.com>
2023-01-03 01:24:37 -0500

    update compile test to run setup_exe

--- backend/tests/compile_test.py
+++ backend/tests/compile_test.py
@@ -7,7 +7,7 @@ from setuptools import sandbox
 class CompileTest(unittest.TestCase):
     def test_msi_dist_compile(self):
         if system() == "Windows":
-            sandbox.run_setup("setup_msi.py", ["bdist_msi"])
+            sandbox.run_setup("setup_exe.py", ["build"])
 
 
 if __name__ == "__main__":
