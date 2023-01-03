c0a8f44765b7f717e8d8c429eacffb085571676b
Martin Miglio <marmig0404@gmail.com>
2023-01-03 01:00:33 -0500

    move old main to debug_gui

--- backend/pyclashbot/debug/debug_gui.py
+++ backend/pyclashbot/debug/debug_gui.py
@@ -1,63 +1,166 @@
-"""a dummy test function to simulate the interface"""
-
-import time
+import sys
 import webbrowser
-from typing import Any
 
 import PySimpleGUI as sg
 
-from pyclashbot.debug.debug_layout import disable_keys, main_layout
-from pyclashbot.interface.help import show_help_gui
-from pyclashbot.interface.theme import THEME
+from pyclashbot.bot import WorkerThread
+from pyclashbot.interface import (
+    disable_keys,
+    main_layout,
+    show_help_gui,
+    user_config_keys,
+)
+from pyclashbot.utils import Logger, cache_user_settings, read_user_settings
+from pyclashbot.utils.caching import check_user_settings
+from pyclashbot.utils.thread import PausableThread, StoppableThread
 
-sg.theme(THEME)
 
-if __name__ == "__main__":
+def read_window(
+    window: sg.Window, timeout: int = 10
+) -> tuple[str, dict[str, str | int]]:
+    # Method for reading the attributes of the window
+    # have a timeout so the output can be updated when no events are happening
+    read_result = window.read(timeout=timeout)  # ms
+    if read_result is None:
+        print("Window not found")
+        sys.exit()
+    return read_result
 
-    # some sample statistics
-    statistics: dict[str, Any] = {
-        "current_status": "Idle",
-        "time_since_start": "00:00:00",
-        "wins": 1,
-        "losses": 2,
-        "fights": 3,
-        "requests": 4,
-        "restarts": 5,
-        "chests_unlocked": 6,
-        "cards_played": 7,
-        "cards_upgraded": 8,
-        "account_switches": 9,
-        "card_mastery_reward_collections": 10,
-        "battlepass_rewards_collections": 11,
-        "level_up_chest_collections": 12,
-        "war_battles_fought": 13,
-        "free_offer_collections": 14,
-    }
+
+def read_job_list(values: dict[str, str | int]) -> list[str]:
+    jobs = []
+    if values["-Open-Chests-in-"]:
+        jobs.append("Open Chests")
+    if values["-Fight-in-"]:
+        jobs.append("Fight")
+    if values["-Requesting-in-"]:
+        jobs.append("Request")
+    if values["-Upgrade_cards-in-"]:
+        jobs.append("Upgrade")
+    if values["-Random-Decks-in-"]:
+        jobs.append("Randomize Deck")
+    if values["-Card-Mastery-Collection-in-"]:
+        jobs.append("card mastery collection")
+    if values["-Level-Up-Reward-Collection-in-"]:
+        jobs.append("level up reward collection")
+    if values["-Battlepass-Reward-Collection-in-"]:
+        jobs.append("battlepass reward collection")
+    if values["-War-Participation-in-"]:
+        jobs.append("war")
+    if values["-Free-Offer-Collection-in-"]:
+        jobs.append("free offer collection")
+    if values["-Daily-Challenge-Reward-Collection-"]:
+        jobs.append("daily challenge reward collection")
+
+    return jobs
+
+
+def save_current_settings(values):
+    # read the currently selected values for each key in user_coinfig_keys
+    user_settings = {key: values[key] for key in user_config_keys if key in values}
+    # cache the user settings
+    cache_user_settings(user_settings)
+
+
+def load_last_settings(window):
+    if check_user_settings():
+        read_window(window)  # read the window to edit the layout
+        user_settings = read_user_settings()
+        if user_settings is not None:
+            for key in user_config_keys:
+                if key in user_settings:
+                    window[key].update(user_settings[key])
+        window.refresh()  # refresh the window to update the layout
+
+
+def start_button_event(logger: Logger, window, values):
+    logger.change_status("Start Button Event")
+
+    for key in disable_keys:
+        window[key].update(disabled=True)
+
+    jobs = read_job_list(values)
+
+    # check if at least one job is selected
+    if len(jobs) == 0:
+        logger.change_status("At least one job must be selected")
+        return None
+
+    # setup thread and start it
+    acc_count = int(values["-SSID_IN-"])
+    args = (jobs, acc_count)
+    thread = WorkerThread(logger, args)
+    thread.start()
+
+    # enable the stop button after the thread is started
+    window["Stop"].update(disabled=False)
+    window["-Pause-Resume-Button-"].update(text="Pause")
+    window["-Pause-Resume-Button-"].update(disabled=False)
+
+    return thread
+
+
+def stop_button_event(logger: Logger, window, thread: StoppableThread):
+    logger.change_status("Stopping")
+    window["Stop"].update(disabled=True)
+    window["-Pause-Resume-Button-"].update(text="Pause")
+    window["-Pause-Resume-Button-"].update(disabled=True)
+    thread.shutdown()  # send the shutdown flag to the thread
+
+
+def pause_resume_button_event(logger: Logger, window, thread: PausableThread):
+    if thread.toggle_pause():
+        logger.change_status("Pausing")
+        window["-Pause-Resume-Button-"].update(text="Resume")
+    else:
+        logger.change_status("Resuming")
+        window["-Pause-Resume-Button-"].update(text="Pause")
+
+
+def update_layout(window: sg.Window, logger: Logger):
+    window["time_since_start"].update(logger.calc_time_since_start())  # type: ignore
+    # update the statistics in the gui
+    stats = logger.get_stats()
+    if stats is not None:
+        for stat, val in stats.items():
+            window[stat].update(val)  # type: ignore
+
+
+def main_gui():
+    console_log = True  # enable/disable console logging
 
     window = sg.Window("Py-ClashBot", main_layout)
 
-    running = False  # pylint: disable=invalid-name
+    load_last_settings(window)
 
+    # track worker thread and logger
+    thread: WorkerThread | None = None
+    logger = Logger(console_log=console_log, timed=False)
+
+    # run the gui
     while True:
-        event, values = window.read(timeout=100)  # type: ignore
-        if event == sg.WIN_CLOSED:
+        event, values = read_window(window, timeout=10)
+
+        if event in [sg.WIN_CLOSED, "Exit"]:
+            # shut down the thread if it is still running
+            if thread is not None:
+                thread.shutdown()
             break
+
         if event == "Start":
-            window["current_status"].update("Starting")  # type: ignore
-            for key in disable_keys:
-                window[key].update(disabled=True)
-            running = True  # pylint: disable=invalid-name
-            window["Stop"].update(disabled=False)
 
-        elif event == "Stop":
-            window["current_status"].update("Stopping")  # type: ignore
-            running = False  # pylint: disable=invalid-name
-            for key in disable_keys:
-                window[key].update(disabled=False)
-            window["Stop"].update(disabled=True)
+            # reset the logger for a new thread
+            logger = Logger(console_log=console_log)
+            thread = start_button_event(logger, window, values)
 
-        elif event == "Help":
-            show_help_gui()
+        elif event == "Stop" and thread is not None:
+            stop_button_event(logger, window, thread)
+
+        elif event == "-Pause-Resume-Button-" and thread is not None:
+            pause_resume_button_event(logger, window, thread)
+
+        elif event in user_config_keys:
+            save_current_settings(values)
 
         elif event == "Donate":
             webbrowser.open(
@@ -68,23 +171,36 @@ if __name__ == "__main__":
                 + "&currency_code=USD"
             )
 
+        elif event == "Help":
+            show_help_gui()
+
         elif event == "issues-link":
             webbrowser.open(
                 "https://github.com/matthewmiglio/py-clash-bot/issues/new/choose"
             )
 
-        # if running:
-        # pass
+        # handle when thread is finished
+        if thread is not None and not thread.is_alive():
+            # enable the start button and configuration after the thread is stopped
+            for key in disable_keys:
+                window[key].update(disabled=False)
+            if thread.logger.errored:
+                window["Stop"].update(disabled=True)
+                window["-Pause-Resume-Button-"].update(disabled=True)
+            else:
+                # reset the logger
+                logger = Logger(console_log=console_log, timed=False)
+                thread = None
 
-        # update the statistics
-        # window["current_status"].update(statistics["current_status"]) # type: ignore
-        # window["time_since_start"].update(statistics["time_since_start"])  # type: ignore
-        # window["wins"].update(statistics["wins"])
-        # window["losses"].update(statistics["losses"])
-        # window["fights"].update(statistics["fights"])
-        # window["war_battles_fought"].update(statistics["war_battles_fought"])
-        # window["requests"].update(statistics["requests"])
+        update_layout(window, logger)
 
-        time.sleep(1)
+    # shut down the thread if it is still running
+    if thread is not None:
+        thread.shutdown()
+        thread.join()
 
     window.close()
+
+
+if __name__ == "__main__":
+    main_gui()
