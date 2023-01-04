b1982fb4516e120a9afe5bb847fbae8411da0e07
Martin Miglio <marmig0404@gmail.com>
2023-01-04 14:21:14 -0500

    drop web-vitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./react/App";
-import reportWebVitals from "./reportWebVitals";
 const { ipcRenderer } = window.require("electron");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -24,8 +23,3 @@ new MutationObserver((mutations) => {
   childList: true,
   subtree: true,
 });
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
