7f1444ca837c956c43641eb855ada504ff1d8473
Martin Miglio <marmig0404@gmail.com>
2023-01-17 02:01:42 -0500

    Migrate Docs to NextJS (#136)
    * migrate to next js
    
    * fix ga cookies
    
    
    Former-commit-id: 63ab8232e31a82f0779d8500f7fa0956014dcd5d

--- docs/src/components/readme/ReadMe.js
+++ docs/src/components/readme/ReadMe.js
@@ -1,12 +1,13 @@
 /* eslint-disable max-len */
 import React, {Component} from 'react';
+import Image from 'next/image'
 import {handleClick} from '../../GoogleAnalytics';
 import ReleaseLink from '../release/ReleaseLink';
 import Badge from '../badge/Badge';
 import logo from '../../assets/pixel-pycb.svg';
 import demogame from '../../assets/demo-game.webp';
 import demogui from '../../assets/demo-gui.webp';
-import './ReadMe.css';
+import styles from './ReadMe.module.css';
 
 /**
  * Component for project description
@@ -18,26 +19,26 @@ export default class ReadMe extends Component {
    */
   render() {
     return (
-      <div className="ReadMe">
-        <div className='block'>
-          <div className='header-block'>
-            <div className='text-block'>
+      <div className={styles.ReadMe} >
+        <div className={styles.block}>
+          <div className={styles.headerblock}>
+            <div className={styles.textblock}>
               <h1>py-clash-bot</h1>
               <Badge/>
             </div>
-            <div className='logo-block'>
-              <img className='logo' alt="py-clash-bot logo" src={logo}
+            <div className={styles.logoblock}>
+              <Image className={styles.logo} alt="py-clash-bot logo" src={logo}
                 content="max-age=259200" />
             </div>
           </div>
-          <p className='description'>
+          <p className={styles.description}>
                 py-clash-bot is an open-source application that allows users to automate
                 their Clash Royale gameplay on Windows using an emulated Android phone. The
                 bot uses a combination of image recognition, mouse control, and Android
                 emulation to perform a variety of tasks without user intervention.
           </p>
         </div>
-        <div className='block'>
+        <div className={styles.block}>
           <h2>Features</h2>
           <ul>
             <li>Automated level-up, clan wars and donations</li>
@@ -45,11 +46,11 @@ export default class ReadMe extends Component {
             <li>Multi-account support</li>
           </ul>
         </div>
-        <div className='block'>
+        <div className={styles.block}>
           <h2>Installation</h2>
           <ol>
             <li>
-              <ReleaseLink child={<button className='download-button'>Download</button>} /> and run the latest windows installer
+              <ReleaseLink child={<button className={styles.downloadbutton}>Download</button>} /> and run the latest windows installer
             </li>
             <li>Run the shortcut on the desktop</li>
             <li>
@@ -58,7 +59,7 @@ export default class ReadMe extends Component {
             </li>
           </ol>
         </div>
-        <div className='block'>
+        <div className={styles.block}>
           <h2>Usage</h2>
           <ol>
             <li>Configure the jobs to perform, and number of accounts</li>
@@ -71,14 +72,14 @@ export default class ReadMe extends Component {
             </li>
           </ol>
         </div>
-        <div className='block'>
+        <div className={styles.block}>
           <h2>Demo</h2>
-          <div className="demo">
-            <img className='demo-game' alt="Clash Royale gameplay" src={demogame}/>
-            <img className='demo-gui' alt="User interface" src={demogui} />
+          <div className={styles.demo}>
+            <Image className={styles.demoimage} alt="Clash Royale gameplay" src={demogame}/>
+            <Image className={styles.demoimage} alt="User interface" src={demogui} />
           </div>
         </div>
-        <div className='block'>
+        <div className={styles.block}>
           <h2>Contribute</h2>
           <p>
           We welcome contributions to the py-clash-bot project. If you have an idea for a
