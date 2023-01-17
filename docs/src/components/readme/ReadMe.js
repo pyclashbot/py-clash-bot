4a4b1b7a2ff5874d73c189b5facb37c43bf7a4c0
Martin Miglio <marmig0404@gmail.com>
2023-01-17 15:10:10 -0500

    Improve docs performance (#138)
    dynamically load components
    use nextjs image elements
    remove unused deps
    add icon to page
    
    
    Former-commit-id: 52ab05b34232ed7a90aca8b00dad4599bba2bc90

--- docs/src/components/readme/ReadMe.js
+++ docs/src/components/readme/ReadMe.js
@@ -1,13 +1,18 @@
 /* eslint-disable max-len */
-import React, {Component} from 'react';
-import Image from 'next/image'
-import {handleClick} from '../../GoogleAnalytics';
-import ReleaseLink from '../release/ReleaseLink';
-import Badge from '../badge/Badge';
-import logo from '../../assets/pixel-pycb.svg';
-import demogame from '../../assets/demo-game.webp';
-import demogui from '../../assets/demo-gui.webp';
-import styles from './ReadMe.module.css';
+import React, { Component } from "react";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+import { handleClick } from "../../GoogleAnalytics";
+
+import logo from "../../assets/pixel-pycb.svg";
+import demogame from "../../assets/demo-game.webp";
+import demogui from "../../assets/demo-gui.webp";
+
+import styles from "./ReadMe.module.css";
+
+const ReleaseLink = dynamic(() => import("../release/ReleaseLink"));
+const Badge = dynamic(() => import("../badge/Badge"));
 
 /**
  * Component for project description
@@ -19,23 +24,28 @@ export default class ReadMe extends Component {
    */
   render() {
     return (
-      <div className={styles.ReadMe} >
+      <div className={styles.ReadMe}>
         <div className={styles.block}>
           <div className={styles.headerblock}>
             <div className={styles.textblock}>
               <h1>py-clash-bot</h1>
-              <Badge/>
+              <Badge />
             </div>
             <div className={styles.logoblock}>
-              <Image className={styles.logo} alt="py-clash-bot logo" src={logo}
-                content="max-age=259200" />
+              <Image
+                className={styles.logo}
+                alt="py-clash-bot logo"
+                src={logo}
+                content="max-age=259200"
+              />
             </div>
           </div>
           <p className={styles.description}>
-                py-clash-bot is an open-source application that allows users to automate
-                their Clash Royale gameplay on Windows using an emulated Android phone. The
-                bot uses a combination of image recognition, mouse control, and Android
-                emulation to perform a variety of tasks without user intervention.
+            py-clash-bot is an open-source application that allows users to
+            automate their Clash Royale gameplay on Windows using an emulated
+            Android phone. The bot uses a combination of image recognition,
+            mouse control, and Android emulation to perform a variety of tasks
+            without user intervention.
           </p>
         </div>
         <div className={styles.block}>
@@ -50,12 +60,17 @@ export default class ReadMe extends Component {
           <h2>Installation</h2>
           <ol>
             <li>
-              <ReleaseLink child={<button className={styles.downloadbutton}>Download</button>} /> and run the latest windows installer
+              <ReleaseLink
+                child={
+                  <button className={styles.downloadbutton}>Download</button>
+                }
+              />{" "}
+              and run the latest windows installer
             </li>
             <li>Run the shortcut on the desktop</li>
             <li>
-            The program will automatically install MEmu and AutoHotKey, follow the
-            installation prompts
+              The program will automatically install MEmu and AutoHotKey, follow
+              the installation prompts
             </li>
           </ol>
         </div>
@@ -64,34 +79,55 @@ export default class ReadMe extends Component {
           <ol>
             <li>Configure the jobs to perform, and number of accounts</li>
             <li>
-            Click the <code>Start</code> button to begin automation
+              Click the <code>Start</code> button to begin automation
             </li>
             <li>
-            Enjoy the benefits of always-on farming with the py-clash-bot taking care
-            of the heavy lifting for you
+              Enjoy the benefits of always-on farming with the py-clash-bot
+              taking care of the heavy lifting for you
             </li>
           </ol>
         </div>
         <div className={styles.block}>
           <h2>Demo</h2>
           <div className={styles.demo}>
-            <Image className={styles.demoimage} alt="Clash Royale gameplay" src={demogame}/>
-            <Image className={styles.demoimage} alt="User interface" src={demogui} />
+            <Image
+              className={styles.demoimage}
+              alt="Clash Royale gameplay"
+              src={demogame}
+            />
+            <Image
+              className={styles.demoimage}
+              alt="User interface"
+              src={demogui}
+            />
           </div>
         </div>
         <div className={styles.block}>
           <h2>Contribute</h2>
           <p>
-          We welcome contributions to the py-clash-bot project. If you have an idea for a
-          new feature or have found a bug, please open an issue on the{' '}
-            <a href="https://github.com/matthewmiglio/py-clash-bot/issues" onClick={handleClick}>issues tab</a>{' '}
-          or submit a pull request.
+            We welcome contributions to the py-clash-bot project. If you have an
+            idea for a new feature or have found a bug, please open an issue on
+            the{" "}
+            <a
+              href="https://github.com/matthewmiglio/py-clash-bot/issues"
+              onClick={handleClick}
+            >
+              issues tab
+            </a>{" "}
+            or submit a pull request.
           </p>
-          <p>The source code can be viewed on{' '}
-            <a href="https://github.com/matthewmiglio/py-clash-bot" onClick={handleClick}>GitHub</a>.
+          <p>
+            The source code can be viewed on{" "}
+            <a
+              href="https://github.com/matthewmiglio/py-clash-bot"
+              onClick={handleClick}
+            >
+              GitHub
+            </a>
+            .
           </p>
         </div>
-      </div >
+      </div>
     );
   }
 }
