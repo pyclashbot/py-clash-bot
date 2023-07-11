0d8e150b1a37b213cd620f00c002585e83d1faca
Matt Main_PC <matmigg0804@gmail.com>
2023-07-11 11:02:02 -0400

    implementing chest queueing when opening chests
    Former-commit-id: 69d3e2d1400695fad83ec8aa486442bc39a35fa6

--- src/pyclashbot/bot/open_chests_state.py
+++ src/pyclashbot/bot/open_chests_state.py
@@ -6,9 +6,52 @@ from pyclashbot.bot.nav import (
     check_if_on_clash_main_menu,
     handle_clash_main_tab_notifications,
 )
-from pyclashbot.detection.image_rec import check_line_for_color, pixel_is_equal
+from pyclashbot.detection.image_rec import (
+    check_line_for_color,
+    pixel_is_equal,
+    region_is_color,
+)
 from pyclashbot.memu.client import click, screenshot
 from pyclashbot.utils.logger import Logger
+
+UNLOCK_CHEST_BUTTON_COORD = (207, 412)
+QUEUE_CHEST_BUTTON_COORD = (314, 357)
+
+
+def open_chests_state(vm_index, logger: Logger, next_state: str):
+    logger.change_status(status="Opening chests state")
+
+    logger.change_status(status="Handling obstructing notifications")
+    if handle_clash_main_tab_notifications(vm_index, logger) == "restart":
+        logger.change_status(
+            status="Error 07531083150 Failure with handle_clash_main_tab_notifications"
+        )
+        return "restart"
+
+    time.sleep(3)
+
+    # if not on clash main return
+    if not check_if_on_clash_main_menu(vm_index):
+        logger.change_status(
+            status="ERROR 827358235 Not on clash main menu, returning to start state"
+        )
+        return "restart"
+
+    logger.change_status(status="Opening chests...")
+    # check which chests are available
+    statuses = get_chest_statuses(vm_index)  # available/unavailable
+
+    chest_index = 0
+    for status in statuses:
+        logger.log(f'Investigating chest #{chest_index} with status "{status}"')
+        if status == "available":
+            open_chest(vm_index, logger, chest_index)
+
+        chest_index += 1
+
+    time.sleep(3)
+
+    return next_state
 
 
 def get_chest_statuses(vm_index):
@@ -67,42 +110,6 @@ def get_chest_statuses(vm_index):
     return statuses
 
 
-def open_chests_state(vm_index, logger: Logger, next_state: str):
-    logger.change_status(status="Opening chests state")
-
-    logger.change_status(status="Handling obstructing notifications")
-    if handle_clash_main_tab_notifications(vm_index, logger) == "restart":
-        logger.change_status(status=
-            "Error 07531083150 Failure with handle_clash_main_tab_notifications"
-        )
-        return "restart"
-
-    time.sleep(3)
-
-    # if not on clash main return
-    if not check_if_on_clash_main_menu(vm_index):
-        logger.change_status(status=
-            "ERROR 827358235 Not on clash main menu, returning to start state"
-        )
-        return "restart"
-
-    logger.change_status(status="Opening chests...")
-    # check which chests are available
-    statuses = get_chest_statuses(vm_index)  # available/unavailable
-
-    chest_index = 0
-    for status in statuses:
-        logger.log(f'Investigating chest #{chest_index} with status "{status}"')
-        if status == "available":
-            open_chest(vm_index, logger, chest_index)
-
-        chest_index += 1
-
-    time.sleep(3)
-
-    return next_state
-
-
 def open_chest(vm_index, logger, chest_index):
     chest_coords = [
         (77, 497),
@@ -119,11 +126,39 @@ def open_chest(vm_index, logger, chest_index):
     # if its unlockable, unlock it
     if check_if_chest_is_unlockable(vm_index):
         logger.add_chest_unlocked()
-        click(vm_index, 207, 412)
+        click(vm_index, UNLOCK_CHEST_BUTTON_COORD[0], UNLOCK_CHEST_BUTTON_COORD[1])
+        time.sleep(3)
+
+    if check_if_can_queue_chest(vm_index):
+        click(vm_index, QUEUE_CHEST_BUTTON_COORD[0], QUEUE_CHEST_BUTTON_COORD[1])
         time.sleep(3)
 
     # click deadspace a bunch if you just accidentally opened the chest
     click(vm_index, 20, 350, clicks=20, interval=0.3)
+
+
+def check_if_can_queue_chest(vm_index):
+    if not check_line_for_color(vm_index, 293, 345, 301, 354, (255, 255, 255)):
+        return False
+    if not check_line_for_color(vm_index, 338, 345, 329, 357, (255, 255, 255)):
+        return False
+    if not check_line_for_color(vm_index, 336, 369, 329, 358, (255, 255, 255)):
+        return False
+
+    if not region_is_color(
+        vm_index,
+        [
+            280,
+            360,
+            16,
+            8,
+        ],
+        (255, 188, 41),
+    ):
+        return False
+    if not region_is_color(vm_index, [342, 354, 12, 16], (255, 188, 43)):
+        return False
+    return True
 
 
 def check_if_chest_is_unlockable(vm_index):
