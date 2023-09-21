cd89f6dd3fa3bdce6b19bbd0ce753881f6ca513c
Matt Main_PC <matmigg0804@gmail.com>
2023-09-20 23:06:42 -0400

    fixing setup_msi.py to bring donate image files

--- src/setup_msi.py
+++ src/setup_msi.py
@@ -21,7 +21,16 @@ except (ValueError, IndexError):
 
 build_exe_options = {
     "excludes": ["test", "setuptools"],
-    "include_files": ["..\\assets\\pixel-pycb.ico"],
+    "include_files": [
+        "..\\assets\\pixel-pycb.ico",
+        'pyclashbot\\interface\\assets\\donate1.png',
+        'pyclashbot\\interface\\assets\\donate2.png',
+        'pyclashbot\\interface\\assets\\donate3.png',
+        'pyclashbot\\interface\\assets\\donate4.png',
+        'pyclashbot\\interface\\assets\\donate5.png',
+        'pyclashbot\\interface\\assets\\donate6.png',
+        'pyclashbot\\interface\\assets\\donate7.png',
+                      ],
     "include_msvcr": True,
 }
 
