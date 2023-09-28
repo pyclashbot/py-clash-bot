419b81eef652a12f9997321260d68e9018f85885
Matthew <matmigg0804@gmail.com>
2023-09-28 06:25:38 -0400

    fixing check_enemy_tower_statuses()
    Former-commit-id: 30a419e3804684359b9f8cfc44a141aba4676d8b

--- src/pyclashbot/bot/do_fight_state.py
+++ src/pyclashbot/bot/do_fight_state.py
@@ -1,4 +1,5 @@
 """random module for randomizing fight plays"""
+import numpy
 import random
 import time
 from typing import Literal
@@ -16,8 +17,8 @@ from pyclashbot.bot.nav import (
     wait_for_2v2_battle_start,
     wait_for_clash_main_menu,
 )
-from pyclashbot.detection.image_rec import check_line_for_color, region_is_color
-from pyclashbot.memu.client import click, scroll_up, scroll_up_on_left_side_of_screen
+from pyclashbot.detection.image_rec import check_line_for_color, pixel_is_equal, region_is_color
+from pyclashbot.memu.client import click, screenshot, scroll_up, scroll_up_on_left_side_of_screen
 from pyclashbot.utils.logger import Logger
 
 LEAVE_1V1_BATTLE_OK_BUTTON: tuple[Literal[210], Literal[554]] = (210, 554)
@@ -194,7 +195,6 @@ def do_1v1_fight_state(vm_index, logger: Logger, next_state):
 def do_2v2_fight_state(vm_index, logger: Logger, next_state):
     """method to handle the entirety of the 2v2 battle state (start fight, do fight, end fight)"""
 
-    logger.change_status(status="do_1v1_fight_state state")
     logger.change_status(status="waiting for 2v2 battle start")
 
     # wait for battle start
@@ -304,18 +304,24 @@ def _2v2_fight_loop(vm_index, logger: Logger) -> Literal["restart", "good"]:
 
         # choose random card to play
         random_card_index = random.randint(0, 3)
-        logger.log(f"Clicking card index {random_card_index}")
+        logger.change_status(f'Gonna play card index #{random_card_index}')
+
 
         # choose play coord but favor a side according to favorite_side var
+        play_choice_start_time = time.time()
         this_play_side = choose_play_side(vm_index, favorite_side)
+        logger.change_status(f'Choose a play side in {str(time.time() - play_choice_start_time)[:4]}s')
 
         # get a coord based on the selected side
+        play_coords_start_time = time.time()
         card_id, play_coord = get_play_coords_for_card(
             vm_index, random_card_index, this_play_side
         )
+        logger.change_status(f'Choose a play coord in {str(time.time() - play_coords_start_time)[:4]}s')
 
         # if coord is none for whatever reason, just skip this play
         if play_coord is None:
+            logger.change_status('Bad play coord. Redoing...')
             continue
 
         id_string = "Regular card"
@@ -435,17 +441,27 @@ def wait_for_4_elixer(vm_index, logger, mode="1v1"):
     """method to wait for 4 elixer during a battle"""
 
     start_time = time.time()
-    while not check_for_4_elixer(vm_index):
+   
+    while 1:
+        logger.change_status(f'Waiting for 4 elixer for {str(time.time() - start_time)}s...')
+
+        if check_for_4_elixer(vm_index):
+            break
+        
         if time.time() - start_time > ELIXER_WAIT_TIMEOUT:
             return "restart"
 
         if mode == "1v1" and not check_for_in_1v1_battle(vm_index):
-            logger.change_status(status="Not in battle, stopping waiting for 4 elixer")
+            logger.change_status(status="Not in battle, stopping waiting for 4 elixer.")
             return "no battle"
         if mode == "2v2" and not check_for_in_2v2_battle(vm_index):
-            logger.change_status(status="Not in battle, stopping waiting for 4 elixer")
+            logger.change_status(status="Not in battle, stopping waiting for 4 elixer.")
             return "no battle"
-    return "good"
+    
+
+    logger.change_status(f'Took {str(time.time() - start_time)[:4]}s for 4 elixer.')
+
+    return True
 
 
 def check_for_4_elixer(vm_index):
@@ -498,20 +514,37 @@ def check_enemy_tower_statuses(
     """method to scan pixels during a battle to determine
     which of  the enemy towers are alive or destroyed"""
 
-    left_tower_color = (232, 188, 43)
-    right_tower_color = (232, 188, 42)
 
-    left_tower_alive = check_line_for_color(
-        vm_index, x_1=89, y_1=92, x_2=101, y_2=101, color=left_tower_color
-    )
-    right_tower_alive = check_line_for_color(
-        vm_index, x_1=276, y_1=90, x_2=289, y_2=102, color=right_tower_color
-    )
 
-    left_status = "alive" if left_tower_alive else "destroyed"
-    right_status = "alive" if right_tower_alive else "destroyed"
+    #'alive'
+    #or
+    #'destroyed'
 
-    return (left_status, right_status)
+
+    iar = numpy.asarray(screenshot(vm_index))
+
+    left_tower_pixel = iar[117][102]
+    right_tower_pixel = iar[118][280]
+
+    left_tower_pixel = [left_tower_pixel[2],left_tower_pixel[1],left_tower_pixel[0],]
+    right_tower_pixel = [right_tower_pixel[2],right_tower_pixel[1],right_tower_pixel[0],]
+
+    print(left_tower_pixel)
+    print(right_tower_pixel)
+
+    left_tower_status = 'destroyed'
+    if pixel_is_equal([187,143,44],left_tower_pixel,tol=30):
+        left_tower_status='alive'
+    
+    right_tower_status = 'destroyed'
+    if pixel_is_equal([232,188,44],right_tower_pixel,tol=30):
+        right_tower_status='alive'
+
+    return (left_tower_status, right_tower_status)
+    
+
+    
+
 
 
 def end_fight_state(vm_index, logger: Logger, next_state):
@@ -730,9 +763,15 @@ def check_for_end_1v1_battle_condition_2(vm_index) -> bool:
 
 
 if __name__ == "__main__":
-    wait_for_2v2_battle_start(1, Logger())
+    while 1:print(check_enemy_tower_statuses(
+    1,
+))
+    
+    
+    
+    # wait_for_2v2_battle_start(1, Logger())
 
  
-    # do_2v2_fight_state(1, Logger(), 'str_next_state')
+    do_2v2_fight_state(1, Logger(), 'str_next_state')
     
     # start_2v2_fight_state(1, Logger())
