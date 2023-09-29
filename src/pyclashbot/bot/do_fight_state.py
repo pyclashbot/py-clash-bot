49d6c7162fb01c35db9bec5dc75052ec834f44b1
Matt Main_PC <matmigg0804@gmail.com>
2023-09-29 16:47:28 -0400

    more cards played stats stuff

--- src/pyclashbot/bot/do_fight_state.py
+++ src/pyclashbot/bot/do_fight_state.py
@@ -398,7 +398,8 @@ def _2v2_fight_loop(vm_index, logger: Logger) -> Literal["restart", "good"]:
             f"Made a play in 2v2 mode in {str(time.time() - this_play_start_time)[:4]}\n"
         )
 
-    logger.remove_card_played(cards_to_remove=random.randint(2, 3))
+    if logger.get_cards_played() != 0:
+        logger.remove_card_played(cards_to_remove=random.randint(0, 1))
     cards_played = logger.get_cards_played()
     logger.change_status(f"Played ~{cards_played - prev_cards_played} cards this fight")
 
@@ -490,7 +491,8 @@ def _1v1_fight_loop(vm_index, logger: Logger) -> Literal["restart", "good"]:
         # increment plays counter
         plays += 1
 
-    logger.remove_card_played(cards_to_remove=random.randint(2, 3))
+    if logger.get_cards_played() != 0:
+        logger.remove_card_played(cards_to_remove=random.randint(0, 1))
     cards_played = logger.get_cards_played()
     logger.change_status(f"Played ~{cards_played - prev_cards_played} cards this fight")
     return "good"
