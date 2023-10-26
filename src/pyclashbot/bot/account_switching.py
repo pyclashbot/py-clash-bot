7fcd39e3a363e09f8abbd0189d3dd66aed52fca2
Matt Main_PC <matmigg0804@gmail.com>
2023-10-26 10:11:42 -0400

    implementing account switching
    Former-commit-id: adb9bcc1676248c3f900867f784bd26a35c811c4

--- src/pyclashbot/bot/account_switching.py
+++ src/pyclashbot/bot/account_switching.py
@@ -17,18 +17,22 @@ def switch_accounts(vm_index, logger, account_index_to_switch_to):
         return False
 
     # click options burger
+    logger.change_status("Opening clash main options menu")
     click(vm_index, 386, 66)
-    time.sleep(2)
+    time.sleep(3)
 
     # click switch SSID button
+    logger.change_status("Clicking switch SSID button")
     click(vm_index, 221, 368)
-    time.sleep(4)
+    time.sleep(8)
 
     # click the account index in question
     account_coord = SSID_COORDS[account_index_to_switch_to]
+    logger.change_status("Clicking the account to switch to...")
     click(vm_index, account_coord[0], account_coord[1])
     time.sleep(4)
 
+    logger.change_status("Waiting for clash main on new account...")
     if wait_for_clash_main_menu(vm_index, logger) is False:
         return False
 
