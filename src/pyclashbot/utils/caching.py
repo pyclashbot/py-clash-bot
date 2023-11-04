506c6216988a7b0e15cfe2c0b694757ad1d4d36a
Martin Miglio <marmig0404@gmail.com>
2023-11-04 14:34:48 -0400

    migrate saving user settings in json over pickle (#329)
    Former-commit-id: 5a34cc85b0c52d4cc6be3927e5513fe374ba0195

--- src/pyclashbot/utils/caching.py
+++ src/pyclashbot/utils/caching.py
@@ -1,5 +1,9 @@
+"""
+A module to cache and load program data to and from the disk
+"""
+import json
 import pickle
-from os import makedirs
+from os import makedirs, remove
 from os.path import exists, expandvars, join
 from typing import Any
 
@@ -11,16 +15,46 @@ top_level = join(expandvars("%appdata%"), MODULE_NAME)
 
 
 def _cache_data(data, file_name) -> None:
-    # a method to cache data to the disk using pickle
+    """a method to cache data to the disk using json"""
     file_path = join(top_level, file_name)
     if not exists(top_level):
         makedirs(top_level)
-    with open(file_path, "wb") as this_file:
-        pickle.dump(data, this_file)
+    with open(file_path, "w", encoding="utf-8") as this_file:
+        json.dump(data, this_file)
 
 
-def _load_data(file_name) -> Any | None:
-    # a method to load data from the disk using pickle
+def _load_data(file_name) -> dict[str, Any]:
+    """a method to load data from the disk using json"""
+    file_path = join(top_level, file_name)
+    if not exists(file_path):
+        return {}
+    with open(file_path, "r", encoding="utf-8") as this_file:
+        try:
+            return json.load(this_file)
+        except json.JSONDecodeError:
+            return {}
+
+
+def cache_user_settings(data: dict[str, Any] | None) -> None:
+    """a method to cache user settings to the disk"""
+    _cache_data(data, "user_settings.json")
+
+
+def read_user_settings() -> dict[str, Any]:
+    """a method to read user settings from the disk"""
+    return _load_data("user_settings.json")
+
+
+def check_user_settings() -> bool:
+    """a method to check if the user settings file exists"""
+    return exists(join(top_level, "user_settings.json"))
+
+
+### The following section is for supporting the old pickle format for user settings ###
+
+
+def _load_data_from_pickle(file_name) -> Any | None:
+    """a method to load data from the disk using pickle"""
     file_path = join(top_level, file_name)
     if not exists(file_path):
         return None
@@ -31,16 +65,22 @@ def _load_data(file_name) -> Any | None:
             return None
 
 
-def cache_user_settings(data: dict[str, Any] | None) -> None:
-    # a method to cache user settings to the disk
-    _cache_data(data, "user_settings.dat")
-
-
-def read_user_settings() -> dict[str, Any] | None:
-    # a method to read user settings from the disk
-    return _load_data("user_settings.dat")
-
-
-def check_user_settings() -> bool:
-    # a method to check if the user settings file exists
+def check_old_user_settings() -> bool:
+    """a method to check if the user settings file exists"""
     return exists(join(top_level, "user_settings.dat"))
+
+
+def migrate_user_settings() -> None:
+    """a method to migrate user settings from the old pickle format to the new json format"""
+    user_settings = _load_data_from_pickle("user_settings.dat")
+    if user_settings is not None:
+        cache_user_settings(user_settings)
+        # remove the old pickle file
+        file_path = join(top_level, "user_settings.dat")
+        if exists(file_path):
+            remove(file_path)
+
+
+# when module is imported, if the user settings file exists, migrate it
+if check_old_user_settings():
+    migrate_user_settings()
