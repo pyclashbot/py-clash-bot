f518f725dabc798985712cbd38b6c87b1a1f3773
Martin Miglio <marmig0404@gmail.com>
2023-12-18 03:10:55 -0500

    use adbblitz for screenshotting (#347)
    Former-commit-id: 7833f0ac005e4bbfb5d97ab76dfc5b0379c50012

--- src/pyclashbot/memu/pmc.py
+++ src/pyclashbot/memu/pmc.py
@@ -3,10 +3,16 @@ This module provides a PyMemuc singleton instance.
 """
 
 import sys
+from os.path import join
 
 from pymemuc import PyMemuc
 
 FROZEN = getattr(sys, "frozen", False)
 
-# pmc = PyMemuc(debug=not FROZEN)
-pmc = PyMemuc(debug=False)
+pmc = PyMemuc(debug=not FROZEN)
+
+adb_path = join(
+    # pylint: disable=protected-access
+    pmc._get_memu_top_level(),
+    "adb.exe",
+)
