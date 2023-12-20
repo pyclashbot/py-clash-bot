edb256944d2366b1147ed51e569321fd84faa054
Matt Main_PC <matmigg0804@gmail.com>
2023-12-19 19:11:55 -0500

    starting level up chest collection file

---
+++ src/pyclashbot/bot/level_up_chest.py
@@ -0,0 +1,27 @@
+
+import numpy
+from pyclashbot.memu.client import screenshot
+
+def check_for_bannerbox_chest(vm_index):
+    iar = numpy.asarray(screenshot(vm_index))
+
+    pixels = [
+        iar[9][9],
+        iar[9][22],
+        iar[12][16],
+        iar[23][16],
+    ]
+
+    for i,p in enumerate(pixels):
+        print(p)
+
+
+def collect_bannerbox_chest():
+
+
+    pass
+
+
+if __name__ == '__main__':
+    vm_index=11
+    check_for_bannerbox_chest(vm_index)
