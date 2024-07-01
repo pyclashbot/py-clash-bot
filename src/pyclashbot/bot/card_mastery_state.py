35ba5b7f0ebc4de0a797d02f341166c86dbee08f
Matt Main_PC <matmigg0804@gmail.com>
2024-07-01 10:52:39 -0400

    fixing detection of card mastery rewards
    Former-commit-id: c769e748089ff18d5077b083777d0252b7cf4ecc

--- src/pyclashbot/bot/card_mastery_state.py
+++ src/pyclashbot/bot/card_mastery_state.py
@@ -6,23 +6,26 @@ from pyclashbot.bot.nav import (
     get_to_card_page_from_clash_main,
     wait_for_clash_main_menu,
 )
+from pyclashbot.detection.image_rec import pixel_is_equal
 from pyclashbot.memu.client import click
 from pyclashbot.utils.logger import Logger
 from pyclashbot.memu.client import screenshot
 
 
 def card_mastery_state(vm_index, logger, next_state):
-    logger.change_status(vm_index,"Going to collect card mastery rewards")
+    logger.change_status(vm_index, "Going to collect card mastery rewards")
 
     if check_if_on_clash_main_menu(vm_index) is not True:
-        logger.change_status(vm_index,
-            'Not on clash main menu for card_mastery_state() returning "restart"'
+        logger.change_status(
+            vm_index,
+            'Not on clash main menu for card_mastery_state() returning "restart"',
         )
         return "restart"
 
     if collect_card_mastery_rewards(vm_index, logger) is False:
-        logger.change_status(vm_index,
-            'Failed somewhere in collect_card_mastery_rewards(), returning "restart"'
+        logger.change_status(
+            vm_index,
+            'Failed somewhere in collect_card_mastery_rewards(), returning "restart"',
         )
         return "restart"
 
@@ -31,36 +34,38 @@ def card_mastery_state(vm_index, logger, next_state):
 
 def collect_card_mastery_rewards(vm_index, logger: Logger) -> bool:
     # get to card page
-    logger.change_status(vm_index,"Collecting card mastery rewards...")
+    logger.change_status(vm_index, "Collecting card mastery rewards...")
     if get_to_card_page_from_clash_main(vm_index, logger) == "restart":
-        logger.change_status(vm_index,
-            "Failed to get to card page to collect mastery rewards! Returning false"
+        logger.change_status(
+            vm_index,
+            "Failed to get to card page to collect mastery rewards! Returning false",
         )
         return False
     time.sleep(3)
 
-    if not card_mastery_rewards_exist(vm_index):
-        logger.change_status(vm_index,"No card mastery rewards to collect.")
+    if not card_mastery_rewards_exist_with_delay(vm_index):
+        logger.change_status(vm_index, "No card mastery rewards to collect.")
         time.sleep(1)
 
     else:
         # while card mastery icon exists:
-        while card_mastery_rewards_exist(vm_index):
-            logger.change_status(vm_index,"Detected card mastery rewards")
+        while card_mastery_rewards_exist_with_delay(vm_index):
+            logger.change_status(vm_index, "Detected card mastery rewards")
             #   click card mastery icon
             collect_first_mastery_reward(vm_index)
-            logger.change_status(vm_index,"Collected a card mastery reward!")
+            logger.change_status(vm_index, "Collected a card mastery reward!")
             logger.add_card_mastery_reward_collection()
             time.sleep(2)
 
     # get to clash main
-    logger.change_status(vm_index,"Returning to clash main menu")
+    logger.change_status(vm_index, "Returning to clash main menu")
     click(vm_index, 243, 600)
 
     # wait for main to appear
     if wait_for_clash_main_menu(vm_index, logger) is False:
-        logger.change_status(vm_index,
-            "Failed to get back to clash main menu from card page! Returning false"
+        logger.change_status(
+            vm_index,
+            "Failed to get back to clash main menu from card page! Returning false",
         )
         return False
 
@@ -89,29 +94,40 @@ def collect_first_mastery_reward(vm_index):
     click(vm_index, 243, 600)
 
 
+def card_mastery_rewards_exist_with_delay(vm_index):
+    timeout = 2  # s
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if card_mastery_rewards_exist(vm_index):
+            return True
+
+    return False
+
+
 def card_mastery_rewards_exist(vm_index):
     # Convert the screenshot to a NumPy array for easier access
     iar = numpy.asarray(screenshot(vm_index))
 
-    # Define the target color, positions to check, and tolerance
-    target_color = numpy.array([57, 9, 236])
-    positions_to_check = [
-        (435, 326),
-        (435, 336),
+    pixels = [
+        iar[437][342],
+        iar[422][369],
+        iar[453][368],
     ]
-    tolerance = 10  # Define how much color variation is acceptable
 
-    # Function to check if a pixel is within tolerance
-    def is_color_within_tolerance(pixel_color, target_color, tolerance):
-        return numpy.all(numpy.abs(pixel_color - target_color) <= tolerance)
+    colors = [
+        [95, 214, 251],
+        [58, 10, 236],
+        [43, 190, 255],
+    ]
 
-    # Check each specified position for the target color within tolerance
-    for pos in positions_to_check:
-        pixel_color = iar[pos[0], pos[1]]
-        if not is_color_within_tolerance(pixel_color, target_color, tolerance):
+    for i, p in enumerate(pixels):
+        if not pixel_is_equal(p, colors[i], tol=15):
             return False
+
     return True
 
 
 if __name__ == "__main__":
-    pass
+    while 1:
+        start_time = time.time()
+        print(card_mastery_rewards_exist_with_delay(0),time.time() - start_time)
