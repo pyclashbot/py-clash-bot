dde0e2e2eed2815ab0f6ef9b8c5f645ec76690fb
Matt Main_PC <matmigg0804@gmail.com>
2025-07-05 17:30:17 -0400

    linting

--- src/pyclashbot/google_play_emulator/gpe.py
+++ src/pyclashbot/google_play_emulator/gpe.py
@@ -12,17 +12,9 @@ EMULATOR_PATH = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Pl
 
 def adb(command):
     """Runs an adb command, prints and returns its output."""
-    # print(f"> adb {command}")
     result = subprocess.run(
         f"adb {command}", shell=True, capture_output=True, text=True
     )
-    # print(result.stdout.strip())
-
-    # if result.stderr:
-        # print("stderr:", result.stderr.strip())
-
-    # if result.returncode != 0:
-        # print(f"[!] Command failed with code {result.returncode}")
 
     return result
 
@@ -32,7 +24,7 @@ def close_clash_royale_app():
     start_time = time.time()
     print("Closing Clash Royale...")
     adb(f"shell am force-stop {CLASH_ROYALE_PACKAGE}")
-    print(F'Closed Clash Royale in {time.time() - start_time:.2f} seconds')
+    print(f"Closed Clash Royale in {time.time() - start_time:.2f} seconds")
 
 
 def start_clash_royale():
@@ -42,7 +34,7 @@ def start_clash_royale():
     output = adb(
         f"shell monkey -p {CLASH_ROYALE_PACKAGE} -c android.intent.category.LAUNCHER 1"
     )
-    print(f'Started Clash Royale in {time.time() - start_time:.2f} seconds')
+    print(f"Started Clash Royale in {time.time() - start_time:.2f} seconds")
 
 
 def is_connected():
@@ -62,7 +54,7 @@ def connect():
 
     result = subprocess.run("adb devices", shell=True, capture_output=True, text=True)
     if "offline" in result.stdout:
-        print('[!] Emulator is offline. Please check the connection.')
+        print("[!] Emulator is offline. Please check the connection.")
 
     elif "localhost:6520" in result.stdout and "device" in result.stdout:
         print("Connected to emulator at localhost:6520")
@@ -106,7 +98,7 @@ def screenshot():
     return img
 
 
-def custom_swipe(x1, y1, x2, y2, duration_ms=300):
+def scroll(x1, y1, x2, y2, duration_ms=300):
     adb(f"shell input swipe {x1} {y1} {x2} {y2} {duration_ms}")
 
 
@@ -179,7 +171,7 @@ def restart_emulator():
         resize_emualtor()
     print("Done resizing emulator")
 
-    print(f'Fully restarted emulator in {time.time() - start_time:.2f} seconds')
+    print(f"Fully restarted emulator in {time.time() - start_time:.2f} seconds")
 
 
 def is_emulator_running():
@@ -206,7 +198,7 @@ def test_screenshot():
         # print(f"Mouse at ({x}, {y}) ({r}, {g}, {b})", end='\r')
 
         if event == cv2.EVENT_LBUTTONDOWN:
-            print(f"[{x},{y}] : [{g},{b},{r}]")
+            print(f"[{x},{y}] : [{b}, {g},{r}]")
 
     cv2.namedWindow(window_name)
     cv2.setMouseCallback(window_name, mouse_handler)
@@ -227,6 +219,5 @@ def valid_screen_size(expected_dims: tuple):
 
 
 if __name__ == "__main__":
-    connect()
-    close_clash_royale_app()
-    start_clash_royale()
+    test_screenshot()
+    # start_clash_royale()
