4a40cf6648d31c4a008ade1d29fb9a24ed6f3d10
Matt Main_PC <matmigg0804@gmail.com>
2026-03-15 15:17:51 -0400

    Fix trailing whitespace in states.py for ruff format

--- pyclashbot/bot/states.py
+++ pyclashbot/bot/states.py
@@ -50,7 +50,6 @@ mode_used_in_1v1 = None
 fight_mode_cycle_index = 0
 
 
-
 def get_next_fight_mode(job_list):
     """Get the next fight mode to use, cycling through enabled modes."""
     global fight_mode_cycle_index
